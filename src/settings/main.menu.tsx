4605337fc60da981a5f1695cbff95151b8286da6
yadian <yadianfp@gmail.com>
2023-04-17 00:19:44 -0400

    Update Menu

--- src/settings/main.menu.tsx
+++ src/settings/main.menu.tsx
@@ -2,6 +2,7 @@ import { IMenu } from '@dfl/mui-react-common';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
 import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
+import { JOB_POSITION_PERMISSIONS } from 'modules/rrhh/settings/job-position/constants/job-position.permissions';
 
 export const MAIN_MENU: IMenu[] = [
   {
@@ -10,8 +11,23 @@ export const MAIN_MENU: IMenu[] = [
       {
         title: 'main_menu.admin.section.general.home',
         path: '/',
-        icon: <HomeOutlinedIcon fontSize='small' />,
+        icon: <HomeOutlinedIcon fontSize='small'/>,
       },
+    ],
+  },
+  {
+    title: 'main_menu.admin.section.rrhh.title',
+    prefix: '/rrhh',
+    permissions: [JOB_POSITION_PERMISSIONS.JOB_POSITION_VIEW],
+    atLessOne: true,
+    items: [
+      {
+        title: 'main_menu.admin.section.rrhh.settings',
+        path: '/rrhh/settings/job-positions',
+        partialMatch: true,
+        icon: <AssignmentIndOutlinedIcon fontSize='small'/>,
+        permissions: [JOB_POSITION_PERMISSIONS.JOB_POSITION_VIEW],
+      }
     ],
   },
   {
@@ -23,14 +39,14 @@ export const MAIN_MENU: IMenu[] = [
       {
         title: 'main_menu.admin.section.security.users',
         path: '/security/users',
-        icon: <AssignmentIndOutlinedIcon fontSize='small' />,
+        icon: <AssignmentIndOutlinedIcon fontSize='small'/>,
         permissions: ['ADMIN'],
       },
       {
         title: 'main_menu.admin.section.security.roles',
         path: '/security/roles',
         partialMatch: true,
-        icon: <SecurityOutlinedIcon fontSize='small' />,
+        icon: <SecurityOutlinedIcon fontSize='small'/>,
         permissions: ['ADMIN'],
       },
     ],
