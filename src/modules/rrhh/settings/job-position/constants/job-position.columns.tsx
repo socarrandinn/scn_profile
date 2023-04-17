41cd320d64153293fc816470a1aa5bf275949a68
yadian <yadianfp@gmail.com>
2023-04-17 00:11:28 -0400

    adding security

--- src/modules/rrhh/settings/job-position/constants/job-position.columns.tsx
+++ src/modules/rrhh/settings/job-position/constants/job-position.columns.tsx
@@ -2,6 +2,7 @@ import { JobPositionRowActions } from 'modules/rrhh/settings/job-position/compon
 import { EditLink, HeadCell } from '@dfl/mui-admin-layout';
 import { IJobPosition } from 'modules/rrhh/settings/job-position/interfaces';
 import { createdATColumn } from 'modules/common/constants/common.columns';
+import { JOB_POSITION_PERMISSIONS } from 'modules/rrhh/settings/job-position/constants/job-position.permissions';
 
 export const jobPositionTitleColumn: HeadCell = {
   field: 'title',
@@ -19,10 +20,10 @@ export const jobPositionActionsColumn: HeadCell = {
   field: 'actions',
   sortable: false,
   width: 100,
+  permissions: JOB_POSITION_PERMISSIONS.JOB_POSITION_WRITE,
   headerName: 'common:actions',
   disablePadding: true,
   component: JobPositionRowActions,
-
 };
 
 export const jobPositionColumns: HeadCell[] = [
--- src/modules/rrhh/settings/job-position/routes/index.ts
+++ src/modules/rrhh/settings/job-position/routes/index.ts
@@ -1,8 +1,11 @@
 import { JobPositionList } from 'modules/rrhh/settings/job-position/pages';
+import { RouteConfig } from '@dfl/react-security';
+import { JOB_POSITION_PERMISSIONS } from 'modules/rrhh/settings/job-position/constants/job-position.permissions';
 
-const routes = {
+const routes: RouteConfig = {
   JobPositionList: {
     path: '/',
+    permissions: JOB_POSITION_PERMISSIONS.JOB_POSITION_VIEW,
     component: JobPositionList,
   },
 };
