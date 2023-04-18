9611f7f58b3a13c1961f506716d1379817d984c5
yadian <yadianfp@gmail.com>
2023-04-17 21:40:16 -0400

    remove hard code

--- src/modules/rrhh/settings/job-position/services/jobPosition.service.ts
+++ src/modules/rrhh/settings/job-position/services/jobPosition.service.ts
@@ -3,4 +3,4 @@ import { IJobPosition } from 'modules/rrhh/settings/job-position/interfaces';
 
 class JobPositionService extends EntityApiService<IJobPosition> {}
 
-export default new JobPositionService('http://localhost:8082/ms-rrhh/api/job-positions');
+export default new JobPositionService('/ms-rrhh/api/job-positions');
