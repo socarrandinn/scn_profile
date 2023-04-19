0a985e0896b90b7ed641821ea31212c44da1f0d6
yadian <yadianfp@gmail.com>
2023-04-19 15:47:11 -0400

    [WIP] employee

--- src/contexts/AppProvider.tsx
+++ src/contexts/AppProvider.tsx
@@ -3,21 +3,27 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ChildrenProps, toasterOptions } from '@dfl/mui-react-common';
 import { useSettings } from 'contexts/SettingsProvider';
 import QueryProvider from 'contexts/QueryContext';
-import { AuthControl, SecurityProvider } from '@dfl/react-security';
+import { AuthControl, SecurityProvider, useSecurity, useUser } from '@dfl/react-security';
 
 type AppContentProps = {
   children: any;
 };
 
 const AppContent = ({ children }: AppContentProps) => {
+  const { isAuthenticated } = useSecurity()
+  const { user } = useUser()
   return (
-    <>
-      <AuthControl />
-      {children}
+        <>
+            <AuthControl/>
+            {children}
 
-      <CssBaseline />
-      <Toaster toastOptions={toasterOptions} />
-    </>
+            <CssBaseline/>
+            <Toaster toastOptions={toasterOptions}/>
+            <div style={{ position: 'fixed', right: 50, bottom: 10 }}>
+                auth: {JSON.stringify(isAuthenticated)}<br/>
+                hasUser: {JSON.stringify(!!user)}
+            </div>
+        </>
   );
 };
 
@@ -25,12 +31,12 @@ export const AppProvider = ({ children }: ChildrenProps) => {
   const { theme } = useSettings(); // App theme
 
   return (
-    <QueryProvider>
-      <ThemeProvider theme={theme}>
-        <SecurityProvider>
-          <AppContent>{children}</AppContent>
-        </SecurityProvider>
-      </ThemeProvider>
-    </QueryProvider>
+        <QueryProvider>
+            <ThemeProvider theme={theme}>
+                <SecurityProvider>
+                    <AppContent>{children}</AppContent>
+                </SecurityProvider>
+            </ThemeProvider>
+        </QueryProvider>
   );
 };
