1c06187eeab4529f785322ba4b16108e3d55051a
Rodolfo Venero Noriega <rveneron@gmail.com>
2023-04-20 11:50:23 -0400

    Redirect to '/' when no route found

--- src/modules/demos/index.tsx
+++ src/modules/demos/index.tsx
@@ -13,7 +13,7 @@ const Module = () => {
         ...containersDemoRoutes,
         ...layoutDemoRoutes
       }}
-      notfoundRedirect={'/demos/buttons'}
+      notfoundRedirect={'/'}
     />
   );
 };
