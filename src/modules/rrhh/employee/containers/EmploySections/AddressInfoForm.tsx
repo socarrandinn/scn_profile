5420b05580377d5468d0554bbff2b25b1510ec39
yadian <yadianfp@gmail.com>
2023-04-21 23:09:25 -0400

    validate address

--- src/modules/rrhh/employee/containers/EmploySections/AddressInfoForm.tsx
+++ src/modules/rrhh/employee/containers/EmploySections/AddressInfoForm.tsx
@@ -5,7 +5,7 @@ const AddressInfoForm = () => {
   return (
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
             <Grid item xs={12}>
-                <AddressInput name={'address'}/>
+                <AddressInput name={'address'} required />
             </Grid>
         </Grid>
   );
