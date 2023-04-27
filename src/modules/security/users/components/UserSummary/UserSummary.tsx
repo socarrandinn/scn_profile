04684ccf95defe68962e726149551ea2ceda415c
Tony <tonynavarro1984@gmail.com>
2023-04-27 13:30:57 -0400

    [fix:security] Avoid lock and verify myself, fix update payload on verify/lock

--- src/modules/security/users/components/UserSummary/UserSummary.tsx
+++ src/modules/security/users/components/UserSummary/UserSummary.tsx
@@ -1,17 +1,26 @@
+import { memo } from 'react';
 import { Divider, Stack } from '@mui/material';
 import UserRoleInfo from 'modules/security/users/components/UserSummary/UserRoleInfo';
-import { memo } from 'react';
 import { UserActions } from 'modules/security/users/components/UserActions';
 import { UserDetail } from 'modules/security/users/components/UserDetail';
-import { PermissionCheck } from '@dfl/react-security';
+import { PermissionCheck, useUser } from '@dfl/react-security';
+import { useParams } from 'react-router-dom';
 
 const UserSummary = () => {
+  const { user } = useUser();
+  const { id } = useParams();
+
   return (
-    <Stack direction={'column'} divider={<Divider orientation='horizontal' light />} spacing={0}>
+    <Stack
+      direction={'column'}
+      divider={<Divider orientation='horizontal' light />}
+      spacing={0}
+      sx={{ paddingBottom: 1 }}
+    >
       <UserDetail />
       <PermissionCheck permissions={'USER_ADMIN'}>
         <UserRoleInfo />
-        <UserActions />
+        {user.id !== id && <UserActions />}
       </PermissionCheck>
     </Stack>
   );
