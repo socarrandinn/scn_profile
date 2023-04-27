4378761353d44a2bbed6a79a56dc4697e3721d5e
yadian <yadianfp@gmail.com>
2023-04-27 16:36:32 -0400

    category

---
+++ src/modules/rrhh/settings/category/components/CategoryRowActions/CategoryRowActions.tsx
@@ -0,0 +1,33 @@
+import { memo } from 'react';
+import { Stack } from '@mui/material';
+import { useToggle } from '@dfl/hook-utils';
+import { useParamsLink } from '@dfl/react-security';
+import { useDeleteCategory } from 'modules/rrhh/settings/category/hooks/useDeleteCategory';
+import { DeleteRowAction, EditRowActions } from '@dfl/mui-admin-layout';
+
+type UserStatusProps = {
+  rowId: string;
+};
+
+const CategoryRowActions = ({ rowId }: UserStatusProps) => {
+  const { isOpen, onClose, onOpen } = useToggle();
+  const handleEdit = useParamsLink({ edit: rowId });
+  const { mutate, isLoading, error } = useDeleteCategory(rowId, onClose);
+  return (
+    <>
+      <Stack direction='row' spacing={1}>
+        <EditRowActions onClick={handleEdit} />
+        <DeleteRowAction
+          isOpen={isOpen}
+          onOpen={onOpen}
+          onClose={onClose}
+          error={error}
+          isLoading={isLoading}
+          onDelete={mutate}
+        />
+      </Stack>
+    </>
+  );
+};
+
+export default memo(CategoryRowActions);
---
+++ src/modules/rrhh/settings/category/interfaces/ICategory.ts
@@ -0,0 +1,8 @@
+export interface ICategory {
+  _id?: string;
+  name: string;
+  icon: string;
+  description?: string;
+  createdAt?: Date;
+  owner?: string;
+}
