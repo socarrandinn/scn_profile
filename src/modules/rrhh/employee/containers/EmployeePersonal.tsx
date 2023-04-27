9ed27ff2a0403e0c529e71a19bbb7e2c007d21a7
yadian <yadianfp@gmail.com>
2023-04-26 21:39:50 -0400

    fix issue

--- src/modules/rrhh/employee/containers/EmployeePersonal.tsx
+++ src/modules/rrhh/employee/containers/EmployeePersonal.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import { EmployeeGeneralInfo } from 'modules/rrhh/employee/components/EmployeeGeneralInfo';
-import { PaperTabView } from 'modules/common/components/TabsWithSections/PaperTabView';
 
 const AccountGeneral = ({ isAccount }: { isAccount?: boolean }) => {
   return (
