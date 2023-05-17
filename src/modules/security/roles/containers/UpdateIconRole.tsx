fd1ef954b91d4519308a2ad7dcf56546a52737af
yadian <yadianfp@gmail.com>
2023-05-16 23:28:44 -0400

    update role icon

--- src/modules/security/roles/containers/UpdateIconRole.tsx
+++ src/modules/security/roles/containers/UpdateIconRole.tsx
@@ -1,24 +1,15 @@
 import { memo, useCallback, useMemo } from 'react';
 import { useRoleDetail } from 'modules/security/roles/contexts';
-import { FontIconPicker, Form, HandlerError } from '@dfl/mui-react-common';
+import { FontIconPicker, Form, FormFontIconPicker, HandlerError } from '@dfl/mui-react-common';
 import useRoleUpdateIconForm from 'modules/security/roles/hooks/useRoleUpdateIconForm';
 
 const UpdateIconRole = () => {
   const { data: role } = useRoleDetail();
-
-  const icon = useMemo(() => {
-    if (role?.icon === 'owner' || role?.icon === 'role4') return 'AssignmentIndIcon';
-    if (role?.icon === 'account' || role?.icon === 'role12') return 'AdminPanelSettingsIcon';
-    if (role?.icon === 'secure' || role?.icon === 'role9') return 'VerifiedUserIcon';
-    return role?.icon;
-  }, [role?.icon]);
-
-  const { isLoading, onSubmit, control, error, setValue } = useRoleUpdateIconForm(role);
+  const { isLoading, onSubmit, error } = useRoleUpdateIconForm(role);
 
   const handleSubmit = useCallback(
     (val: any) => {
-      setValue('avatar', val);
-      onSubmit({ avatar: val } as any).then();
+      onSubmit({ icon: val } as any);
     },
     [onSubmit],
   );
@@ -26,10 +17,7 @@ const UpdateIconRole = () => {
   return (
         <>
             <HandlerError error={error}/>
-            <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'large'}
-                  id={'form-update-icon-to-role'}>
-                <FontIconPicker name='role.icon' defaultValue={icon} size='large' onSubmit={handleSubmit}/>
-            </Form>
+            <FontIconPicker name='icon' value={role?.icon} size='large' onSubmit={handleSubmit}/>
         </>
   );
 };
--- src/modules/security/roles/hooks/useRoleUpdateIconForm.tsx
+++ src/modules/security/roles/hooks/useRoleUpdateIconForm.tsx
@@ -1,45 +1,35 @@
-import {useForm} from 'react-hook-form';
-import {yupResolver} from '@hookform/resolvers/yup';
-import {useMutation, useQueryClient} from '@tanstack/react-query';
-import {RoleService} from 'modules/security/roles/services';
-import {IRole} from 'modules/security/roles/interfaces';
-import {ROLES_LIST_KEY, ROLES_ONE_KEY} from 'modules/security/roles/constants/queries';
-import {roleIconSchema} from 'modules/security/roles/schemas/role.schema';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { RoleService } from 'modules/security/roles/services';
+import { IRole } from 'modules/security/roles/interfaces';
+import { ROLES_LIST_KEY, ROLES_ONE_KEY } from 'modules/security/roles/constants/queries';
+import { roleIconSchema } from 'modules/security/roles/schemas/role.schema';
+import { useEffect } from 'react';
 
 const useRoleUpdateIconForm = (role: IRole | undefined, onClose?: () => void) => {
-    const queryClient = useQueryClient();
-    const {control, handleSubmit, reset, setValue} = useForm<{ avatar: any }>({
-        resolver: yupResolver(roleIconSchema),
-        defaultValues: {avatar: role?.icon},
-    });
+  const queryClient = useQueryClient();
 
-    // @ts-ignore
-    const {mutate, error, isLoading, isSuccess, data} = useMutation<any, any, { avatar: string }>(
-        (avatar: { avatar: string }) => {
-            return RoleService.updateAvatar(role?._id, avatar.avatar);
-        },
-        {
-            onSuccess: () => {
-                queryClient.invalidateQueries([role?._id, ROLES_ONE_KEY]);
-                queryClient.invalidateQueries([ROLES_LIST_KEY]);
-                onClose?.();
-                reset();
-            },
-        },
-    );
+  const { mutate, error, isLoading, isSuccess, data } = useMutation<any, any, { icon: string }>(
+    (icon: { icon: string }) => {
+      return RoleService.updateAvatar(role?._id, icon.icon);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([role?._id, ROLES_ONE_KEY]);
+        queryClient.invalidateQueries([ROLES_LIST_KEY]);
+        onClose?.();
+      },
+    },
+  );
 
-    return {
-        control,
-        error,
-        isLoading,
-        isSuccess,
-        data,
-        reset,
-        setValue,
-        onSubmit: handleSubmit((values) => {
-            mutate(values as any);
-        }),
-    };
+  return {
+    error,
+    isLoading,
+    isSuccess,
+    data,
+    onSubmit: mutate
+  };
 };
 
 export default useRoleUpdateIconForm;
