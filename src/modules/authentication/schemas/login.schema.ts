754581028a885a0bd47bccb6267b43789dbefa68
Angel Labrada Massó <angelitolabm@gmail.com>
2023-05-25 09:41:01 -0400

    fix: Email validation

--- src/modules/authentication/schemas/login.schema.ts
+++ src/modules/authentication/schemas/login.schema.ts
@@ -1,12 +1,21 @@
 import * as Yup from 'yup';
 import '@dfl/yup-validations';
 
+const emailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+Yup.addMethod(Yup.string, 'validEmail', function (errorMessage) {
+  return Yup.string().matches(emailRegex, 'validEmail').required('required');
+});
+
 export const emailScheme = Yup.object().shape({
-  email: Yup.string().email('validEmail').max(255, 'max-255').required('required'),
+  // @ts-ignore
+  email: Yup.string().validEmail(),
 });
 
 export const loginSchema = Yup.object().shape({
-  email: Yup.string().email('validEmail').max(255, 'max-255').required('required'),
+  // @ts-ignore
+  email: Yup.string().validEmail(),
   // @ts-ignore
   password: Yup.string().required('required').password(),
 });
@@ -15,16 +24,19 @@ export const signUpSchema = Yup.object().shape({
   firstName: Yup.string()
     .min(2, 'min-2')
     .max(255, 'max-255')
-  // @ts-ignore
+    // @ts-ignore
     .name('invalidValue')
-    .trim().required('required'),
+    .trim()
+    .required('required'),
   lastName: Yup.string()
     .min(2, 'min-2')
     .max(255, 'max-255')
-  // @ts-ignore
+    // @ts-ignore
     .name('invalidValue')
-    .trim().required('required'),
-  email: Yup.string().email('validEmail').max(255).required('required'),
+    .trim()
+    .required('required'),
+  // @ts-ignore
+  email: Yup.string().validEmail(),
   password: Yup.string()
     .min(6, 'passwordStrength')
     .test('is-not-empty', 'isNotEmpty', (value) => {
