6378d2772fb3ca272dc0056d4fc0d5e13b218c7c
Rodolfo Venero Noriega <rveneron@gmail.com>
2023-05-25 11:27:19 -0400

    [update]: Change color component in update/create team form.

--- src/modules/rrhh/team/components/TeamForm/TeamForm.tsx
+++ src/modules/rrhh/team/components/TeamForm/TeamForm.tsx
@@ -1,5 +1,5 @@
 import { FormEventHandler, memo } from 'react';
-import { Form, FormFontIconPickerLine, FormTextField, HandlerError } from '@dfl/mui-react-common';
+import { Form, FormFontIconPickerLine, FormTextField, HandlerError, FormColorPicker } from '@dfl/mui-react-common';
 import { Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { SelectEmployee } from 'modules/rrhh/employee/common/components/SelectEmployee';
@@ -16,30 +16,34 @@ const TeamForm = ({ error, control, isLoading, onSubmit }: TeamFormProps) => {
   const { t } = useTranslation('team');
 
   return (
-        <div>
-            <HandlerError error={error} errors={TEAM_ERRORS}/>
-            <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'} dark>
-                <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    <Grid item xs={12}>
-                        <FormFontIconPickerLine name='icon' label={t('fields.icon')} shape={'square'} size={'small'}
-                                                previewInLineCount={7}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormTextField fullWidth autoFocus required name='name' label={t('fields.name')}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormTextField fullWidth autoFocus name='color' label={t('fields.color')}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <SelectEmployee name={'manager'} label={t('fields.manager')}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <FormTextField fullWidth multiline minRows={3} name='description'
-                                       label={t('fields.description')}/>
-                    </Grid>
-                </Grid>
-            </Form>
-        </div>
+    <div>
+      <HandlerError error={error} errors={TEAM_ERRORS} />
+      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'} dark>
+        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <Grid item xs={12}>
+            <FormFontIconPickerLine
+              name='icon'
+              label={t('fields.icon')}
+              shape={'square'}
+              size={'small'}
+              previewInLineCount={7}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormTextField fullWidth autoFocus required name='name' label={t('fields.name')} />
+          </Grid>
+          <Grid item xs={12}>
+            <FormColorPicker fullWidth name='color' label={t('fields.color')} />
+          </Grid>
+          <Grid item xs={12}>
+            <SelectEmployee name={'manager'} label={t('fields.manager')} />
+          </Grid>
+          <Grid item xs={12}>
+            <FormTextField fullWidth multiline minRows={3} name='description' label={t('fields.description')} />
+          </Grid>
+        </Grid>
+      </Form>
+    </div>
   );
 };
 
