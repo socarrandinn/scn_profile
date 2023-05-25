16d8be885b778c67b628101711f64e234aa8e4bb
Rodolfo Venero Noriega <rveneron@gmail.com>
2023-05-25 09:06:51 -0400

    [update]: Team Details

--- src/modules/rrhh/employee/management/hooks/useFindEmployeeByTeam.ts
+++ src/modules/rrhh/employee/management/hooks/useFindEmployeeByTeam.ts
@@ -1,11 +1,12 @@
-import UserServices from 'modules/security/users/services/user.services';
 import { useQuery } from '@tanstack/react-query';
 import { useTableRequest } from '@dfl/mui-admin-layout';
+import { EmployeeService } from 'modules/rrhh/employee/management/services';
 
 export const useFindEmployeeByTeam = (teamId: string) => {
-  const { fetch, queryKey } = useTableRequest(UserServices.search, {
+  const { fetch, queryKey } = useTableRequest(EmployeeService.search, {
     field: 'jobInformation.team._id',
     value: teamId,
+    objectId: true,
   });
 
   return useQuery(['members', teamId, queryKey], fetch);
