59a8a0c780a14bdd3df0f57e04d469a006536dd9
yadian <yadianfp@gmail.com>
2023-06-04 23:10:39 -0400

    home layout

--- src/modules/dashboard/pages/DashboardPage.tsx
+++ src/modules/dashboard/pages/DashboardPage.tsx
@@ -4,21 +4,23 @@ import { DetailContent, DetailLayout, DetailSummary } from '@dfl/mui-form-layout
 import { AdvertisementCardList } from 'modules/rrhh/advertisement/components/AdvertisementCardList';
 import { ImportantNotification } from 'modules/dashboard/components/ImportantNotification';
 import { CurrentTask } from 'modules/dashboard/components/CurrentTask';
+import Temporal from 'modules/dashboard/components/Temporal/Temporal';
 
 const DashboardPage = () => {
   return (
-    <PageLayout sx={{ marginY: 3 }}>
-      <DetailLayout>
-        <DetailSummary ghost>
-          <AdvertisementCardList />
-        </DetailSummary>
+        <PageLayout sx={{ marginY: 3 }}>
+            <DetailLayout sx={{ position: 'relative' }}>
+                <DetailSummary ghost>
+                    <AdvertisementCardList/>
+                </DetailSummary>
 
-        <DetailContent ghost>
-            <ImportantNotification/>
-            <CurrentTask/>
-        </DetailContent>
-      </DetailLayout>
-    </PageLayout>
+                <DetailContent ghost>
+                    <ImportantNotification/>
+                    <CurrentTask/>
+                </DetailContent>
+                <Temporal/>
+            </DetailLayout>
+        </PageLayout>
   );
 };
 
