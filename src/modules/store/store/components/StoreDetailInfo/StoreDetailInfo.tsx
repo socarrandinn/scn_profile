c391e41f3ecaedd765cc54321157868035c752df
Daniel <jmastrapa96@gmail.com>
2023-07-10 15:07:34 -0400

    [WIP] Add new Proveedor View para el detail de Store.

--- src/modules/store/store/components/StoreDetailInfo/StoreDetailInfo.tsx
+++ src/modules/store/store/components/StoreDetailInfo/StoreDetailInfo.tsx
@@ -1,10 +1,8 @@
 import React, { memo } from 'react';
 import { useStoreDetail } from 'modules/store/store/context/StoreContext';
 import StoreDetailContentSkeleton from 'modules/store/store/components/StoreDetailInfo/StoreDetailInfoSkeleton';
-import { DetailStack, HandlerError } from '@dfl/mui-react-common';
+import { HandlerError } from '@dfl/mui-react-common';
 import { Stack, Typography } from '@mui/material';
-import { useFindOneUsers } from 'modules/security/users/hooks/useFindOneUsers';
-import { USER_DETAILS_SUMMARY } from 'modules/security/users/constants';
 import { useBreadcrumbName } from '@dfl/mui-admin-layout';
 import { useTranslation } from 'react-i18next';
 import Divider from '@mui/material/Divider';
@@ -12,11 +10,11 @@ import { ContactPreview } from 'modules/store/store/components/ContactPreview';
 import SubSectionTitle from 'modules/common/components/Titles/SubSectionTitle';
 import { AddressValue } from 'modules/common/components/Address';
 import { IAddress } from 'modules/common/interfaces';
+import ProviderView from 'modules/common/components/ProviderView/ProviderView';
 
 const StoreDetailInfo = () => {
   const { t } = useTranslation('store')
   const { store, isLoading, error } = useStoreDetail();
-  const { data } = useFindOneUsers(store?.logistic as string);
   useBreadcrumbName(store?._id || '', store?.name, isLoading);
 
   if (isLoading) {
@@ -27,16 +25,16 @@ const StoreDetailInfo = () => {
   }
   return (
       <Stack p={2} pt={5} spacing={2}>
-        <Stack direction='column' alignItems='center' spacing={0}>
+        <Stack direction='column' alignItems='center' spacing={0} mb={3}>
           <Typography variant={'h3'} mt={1}>
             {store?.name}
           </Typography>
           <Typography color={'text.secondary'}>{store?.description}</Typography>
-          <Typography variant={'h3'} mt={3} >
-            {t('fields.logistic')}
-          </Typography>
         </Stack>
-        <DetailStack details={USER_DETAILS_SUMMARY} data={data} />
+        <Typography variant={'h3'} mt={3} >
+          {t('section.logistic.title')}
+        </Typography>
+        <ProviderView userid={store?.logistic ?? ''}/>
         <Divider sx={{ margin: '15px 0px' }} />
          <ContactPreview contacts={store?.contacts ?? undefined}/>
           <SubSectionTitle>{t('common:address')}</SubSectionTitle>
