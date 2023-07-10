cbe5864fbf94aeeff0df06746acdfe3c54117bb7
Daniel <jmastrapa96@gmail.com>
2023-07-10 16:04:07 -0400

    [WIP] Change type Typography.

--- src/modules/store/store/components/ContactPreview/ContactPreview.tsx
+++ src/modules/store/store/components/ContactPreview/ContactPreview.tsx
@@ -2,9 +2,9 @@ import { memo } from 'react';
 import ContactsEmail from 'modules/store/store/components/ContactPreview/ContactEmail';
 import { useTranslation } from 'react-i18next';
 import SubSectionTitle from 'modules/common/components/Titles/SubSectionTitle';
-import DetailSectionTitle from 'modules/common/components/Titles/DetailSectionTitle';
 import ContactsPhones from 'modules/store/store/components/ContactPreview/ContactPhone';
 import { IContactInfo } from 'modules/store/store/interfaces/IContactInfo';
+import { Typography } from '@mui/material';
 
 type ContactPreviewProps = {
   contacts: IContactInfo | undefined;
@@ -14,7 +14,7 @@ const ContactPreview = ({ contacts }: ContactPreviewProps) => {
   const { t } = useTranslation('store');
   return (
       <div>
-          <DetailSectionTitle>{t('section.contact.title')}</DetailSectionTitle>
+        <Typography variant={'h3'} mt={1} > {t('section.contact.title')}</Typography>
           <SubSectionTitle>{t('fields.contacts.emails')}</SubSectionTitle>
           {
               contacts ? (<ContactsEmail contacts={contacts } />) : (<></>)
