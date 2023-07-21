8c9b2bcd1f9f579907d6616912d72c97b8778e4b
Daniel Mastrapa <jmastrapa96@gmail.com>
2023-07-21 16:15:45 -0400

    [WIP] Delete Component.
    Signed-off-by: Daniel Mastrapa <jmastrapa96@gmail.com>

--- src/modules/store/settings/category/pages/CategoryDetails.tsx
+++ src/modules/store/settings/category/pages/CategoryDetails.tsx
@@ -1,32 +1,11 @@
 import React, { memo } from 'react';
 import { CenterPageLayout } from 'layouts/index';
 import CategoryDetailContainer from '../containers/CategoryDetailContainer';
-import { ContentProgressBar } from 'components/ContentProgressBar';
 
 const CategoryDetails = () => {
   return (
     <CenterPageLayout>
       <CategoryDetailContainer />
-      <ContentProgressBar title={'List Age Distribution'} data={[{
-        value: 62,
-        label: 'COLL'
-      },
-      {
-        value: 98,
-        label: 'COALL'
-      },
-      {
-        value: 6,
-        label: 'aa'
-      },
-      {
-        value: 25,
-        label: 'aaaa'
-      },
-      {
-        value: 32,
-        label: 'AA'
-      }]} />
     </CenterPageLayout>
   );
 };
