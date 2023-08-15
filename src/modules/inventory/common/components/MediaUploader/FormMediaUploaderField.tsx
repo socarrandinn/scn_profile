39325729b848da6a737465377e149fac2c3bd9de
yadian <yadianfp@gmail.com>
2023-08-14 21:14:34 -0400

    react hook form integration

--- src/modules/inventory/common/components/MediaUploader/FormMediaUploaderField.tsx
+++ src/modules/inventory/common/components/MediaUploader/FormMediaUploaderField.tsx
@@ -1,12 +1,12 @@
-import { IImageMedia, IUploadImage } from 'modules/common/interfaces';
-import React, { memo, useCallback, useEffect, useState } from 'react';
-import { useUploadImage, useUploadManyImage } from 'components/UploadFiles/useUploadImage';
+import { IUploadImage } from 'modules/common/interfaces';
+import React, { memo, useCallback, useEffect, useRef } from 'react';
+import { useUploadManyImage } from 'components/UploadFiles/useUploadImage';
 import { FormFieldControl } from '@dfl/mui-react-common';
 import { MediaUploader } from 'modules/inventory/common/components/MediaUploader/index';
 import { COMMON_ERRORS } from 'modules/common/constants';
 
 type FormMediaUploaderFieldProps = {
-  value?: IImageMedia[];
+  value?: IUploadImage[];
   onSuccess?: () => void;
   onChange?: (data: any) => void;
   name: string;
@@ -18,80 +18,77 @@ const imageFileToMedia = (file: File): IUploadImage => ({
   isLoading: true
 });
 
+const transformValue = (value: any) => ({ target: { value } })
+
 const MediaUploaderField = ({ value, onChange, ...props }: FormMediaUploaderFieldProps) => {
   const { mutate, isLoading, error, data, isError } = useUploadManyImage();
-  const [internalValue, setInternalValue] = useState<IUploadImage[]>(value as IUploadImage[]);
+  const currentValue = useRef(value);
 
   useEffect(() => {
-    console.log('value effect', value)
-    setInternalValue(value as IUploadImage[])
-  }, [value]);
+    console.log('update current', value)
+    currentValue.current = value;
+  }, [value])
 
   useEffect(() => {
     console.log('data effect', data)
     let index = 0;
     if (data) {
-      setInternalValue(prevState => {
-        return prevState?.map(item => {
-          if (item.isLoading) {
-            const image = data[index];
-            const isError = !!image.error;
-            index++;
-            return {
-              ...(isError ? item : image),
-              isLoading: false,
-              isError
-            }
+      const newValue = currentValue.current?.map(item => {
+        if (item.isLoading) {
+          const image = data[index];
+          const isError = !!image.error;
+          index++;
+          return {
+            ...(isError ? item : image),
+            isLoading: false,
+            isError
           }
-          return item;
-        })
+        }
+        return item;
       })
+      onChange?.(transformValue(newValue))
     }
-  }, [data])
+  }, [data, onChange])
 
   useEffect(() => {
     console.log('isError effect', isError)
     if (isError) {
-      setInternalValue(prevState => {
-        return prevState?.map(item => {
-          if (item.isLoading) {
-            return {
-              ...item,
-              isLoading: false,
-              isError: true
-            }
+      const newValue = currentValue.current?.map(item => {
+        if (item.isLoading) {
+          return {
+            ...item,
+            isLoading: false,
+            isError: true
           }
-          return item;
-        })
+        }
+        return item;
       })
+      onChange?.(transformValue(newValue))
     }
-  }, [isError])
+  }, [isError, onChange])
 
   const onAcceptFilesHandler = useCallback(
     (files: File[]) => {
       const newFiles = files.map(imageFileToMedia);
-
-      setInternalValue(prevState => {
-        return [...(prevState || []), ...newFiles];
-      });
+      const newValue = [...(currentValue.current || []), ...newFiles];
+      onChange?.(transformValue(newValue))
       mutate(files);
     },
-    [],
+    [onChange],
   );
 
   const deleteImageHandler = useCallback(
-    (index: number, image: IImageMedia) => {
-      setInternalValue(prevState => {
-        return prevState?.filter((_, i) => i !== index)
-      })
+    (index: number) => {
+      const newValue = value?.filter((_, i) => i !== index)
+      onChange?.(transformValue(newValue))
     },
-    [onChange],
+    [value, onChange],
   );
 
   return (
         <div>
             <MediaUploader
-                images={internalValue}
+                images={value}
                 onAcceptFiles={onAcceptFilesHandler}
                 uploading={isLoading}
                 onDeleteImage={deleteImageHandler}
--- src/modules/inventory/product/containers/ProductFormSections/MediaForm.tsx
+++ src/modules/inventory/product/containers/ProductFormSections/MediaForm.tsx
@@ -3,11 +3,11 @@ import { FormMediaUploaderField } from 'modules/inventory/common/components/Medi
 
 const MediaForm = () => {
   return (
-    <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      <Grid item xs={12} md={12}>
-        <FormMediaUploaderField name={'images'} />
-      </Grid>
-    </Grid>
+        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Grid item xs={12} md={12}>
+                <FormMediaUploaderField name={'media'}/>
+            </Grid>
+        </Grid>
   );
 };
 
