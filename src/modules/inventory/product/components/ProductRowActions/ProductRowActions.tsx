f50ab258601690d7019998ca30a1c37aff9e7207
socarrandinn <socarrandin@gmail.com>
2023-12-21 10:06:12 -0500

    [pp] tab product, fix filters

--- src/modules/inventory/product/components/ProductRowActions/ProductRowActions.tsx
+++ src/modules/inventory/product/components/ProductRowActions/ProductRowActions.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 import { Stack } from '@mui/material';
 import { useToggle } from '@dfl/hook-utils';
-
 import { useDeleteProduct } from 'modules/inventory/product/hooks/useDeleteProduct';
 import { DeleteRowAction, EditRowActions } from '@dfl/mui-admin-layout';
 import { useNavigate } from 'react-router';
@@ -13,14 +12,13 @@ type UserStatusProps = {
 const ProductRowActions = ({ rowId }: UserStatusProps) => {
   const { isOpen, onClose, onOpen } = useToggle();
   const navigate = useNavigate();
-
   const { mutate, isLoading, error } = useDeleteProduct(rowId, onClose);
   const goTo = () => {
-    navigate(`/rrhh/products/${rowId}/general`);
+    navigate(`/inventory/products/${rowId}/general`);
   };
   return (
     <>
-      <Stack direction='row' spacing={1}>
+      <Stack direction='row' spacing={1} justifyContent={'center'}>
         <EditRowActions onClick={goTo} />
         <DeleteRowAction
           isOpen={isOpen}
