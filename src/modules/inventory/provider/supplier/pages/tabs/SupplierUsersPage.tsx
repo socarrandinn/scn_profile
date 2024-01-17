ea9cc06a34d953ae4934b281cde67040ae016f04
Padin <padin790@gmail.com>
2024-01-17 09:24:12 -0500

    Added UsersTab with no content yet

---
+++ src/modules/inventory/provider/supplier/pages/tabs/SupplierUsersPage.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react';
+import SupplierGeneralContainer from '../../containers/SupplierGeneralContainer';
+
+const SupplierUsersPage = () => {
+  return <SupplierGeneralContainer />;
+};
+
+export default memo(SupplierUsersPage);
