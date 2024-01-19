7fbe5b7f3932f87affc1ac1ae4f64a94ec81abb5
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-19 10:48:20 -0500

    update product route

--- src/modules/inventory/product/routes/product-details.ts
+++ src/modules/inventory/product/routes/product-details.ts
@@ -1,11 +1,30 @@
 import { RouteConfig } from '@dfl/react-security';
 import SupplierGeneralPage from 'modules/inventory/provider/supplier/pages/tabs/SupplierGeneralPage';
+import ProductGeneralContainer from 'modules/inventory/product/containers/ProductGeneralContainer';
+import ProductInventoryContainer from 'modules/inventory/product/containers/ProductInventoryContainer';
+import ProductPriceContainer from 'modules/inventory/product/containers/ProductPriceContainer';
+import ProductSEOContainer from 'modules/inventory/product/containers/ProductSEOContainer';
 
 const productDetailsRoutes: RouteConfig = {
   general: {
     path: '/general',
-    component: SupplierGeneralPage,
+    component: ProductGeneralContainer,
     data: { tab: 'GENERAL' },
+  },
+  inventory: {
+    path: '/inventory',
+    component: ProductInventoryContainer,
+    data: { tab: 'INVENTARIO' },
+  },
+  prices: {
+    path: '/price',
+    component: ProductPriceContainer,
+    data: { tab: 'PRECIO' },
+  },
+  seo: {
+    path: '/seo',
+    component: ProductSEOContainer,
+    data: { tab: 'SEO' },
   },
   address: {
     path: '/work',
