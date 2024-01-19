b5b018bf3b6f677cf8d6ca564aff949f0f16b3f1
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-19 10:59:34 -0500

    delete summary

--- src/modules/inventory/product/containers/ProductDetailContainer.tsx
+++ src/modules/inventory/product/containers/ProductDetailContainer.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
-import { DetailContent, DetailLayout, DetailSummary } from '@dfl/mui-form-layout';
+import { DetailContent, DetailLayout } from '@dfl/mui-form-layout';
 import { ProductDetailsContent } from 'modules/inventory/product/components/ProductDetailsContent';
-import { ProductSummary } from 'modules/inventory/product/components/ProductSummary';
 import { ProductDetailProvider } from 'modules/inventory/product/contexts/ProductDetail';
 import { ProductHeaderDetails } from '../components/ProductHeaderDetails';
 
@@ -9,9 +8,6 @@ const ProductDetailContainer = () => (
   <ProductDetailProvider>
     <ProductHeaderDetails />
     <DetailLayout marginTop={{ xs: 2, md: 3 }}>
-      <DetailSummary>
-        <ProductSummary />
-      </DetailSummary>
       <DetailContent ghost sx={{ '& .MuiBox-root': { paddingTop: 0 } }}>
         <ProductDetailsContent />
       </DetailContent>
