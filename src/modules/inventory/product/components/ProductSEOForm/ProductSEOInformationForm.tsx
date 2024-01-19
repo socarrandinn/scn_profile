6e93b495311f7f97df3e9cb03b0ab8b3bad635dc
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-19 11:09:42 -0500

    update provider translate files

--- src/modules/inventory/product/components/ProductSEOForm/ProductSEOInformationForm.tsx
+++ src/modules/inventory/product/components/ProductSEOForm/ProductSEOInformationForm.tsx
@@ -1,8 +1,7 @@
 import { FormEventHandler, memo } from 'react';
-import { Form, FormTextField, HandlerError, Small, useDFLForm } from '@dfl/mui-react-common';
+import { Form, FormTextField, HandlerError } from '@dfl/mui-react-common';
 import { Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-import SeoPreview from 'components/SeoPreview/SeoPreview';
 
 type ProductSEOInformationFormProps = {
   error: any;
@@ -13,21 +12,12 @@ type ProductSEOInformationFormProps = {
 
 const ProductSEOInformationForm = ({ error, control, isLoading, onSubmit }: ProductSEOInformationFormProps) => {
   const { t } = useTranslation('product');
-  const { watch } = useDFLForm();
-  const title = watch?.('seo.name');
-  const description = watch?.('seo.description');
 
   return (
     <div>
       <HandlerError error={error} />
       <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'}>
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          <Grid item xs={12} md={12}>
-            <Small>{t('section.searchPreview.subtitle')}</Small>
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <SeoPreview title={title} description={description} />
-          </Grid>
           <Grid item xs={12} md={12}>
             <FormTextField
               fullWidth
