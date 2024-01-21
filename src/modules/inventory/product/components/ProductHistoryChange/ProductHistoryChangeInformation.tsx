91a95e7a4b60207222f521980bfed5c98d0fc257
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-21 16:37:55 -0500

    update files translate

--- src/modules/inventory/product/components/ProductHistoryChange/ProductHistoryChangeInformation.tsx
+++ src/modules/inventory/product/components/ProductHistoryChange/ProductHistoryChangeInformation.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 const ProductHistoryChangeInformation = () => {
   const { t } = useTranslation('product');
 
-  return <FormPaper title={t('section.seo.title')}>Historial de cambios</FormPaper>;
+  return <FormPaper title={t('section.hisroryChange.title')}>Historial de cambios</FormPaper>;
 };
 
 export default memo(ProductHistoryChangeInformation);
