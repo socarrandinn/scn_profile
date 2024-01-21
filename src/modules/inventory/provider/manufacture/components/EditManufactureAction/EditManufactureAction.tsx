a7bbc5d6e54f6088b0fe6d0505e261377c97a442
CarlosMansoCid <carlosmanso12@gmail.com>
2024-01-21 14:28:42 -0500

    added the edit manufacturer action

--- src/modules/inventory/provider/manufacture/components/EditManufactureAction/EditManufactureAction.tsx
+++ src/modules/inventory/provider/manufacture/components/EditManufactureAction/EditManufactureAction.tsx
@@ -2,12 +2,20 @@ import { Button } from '@mui/material';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import { useSearchParamsChange } from '@dfl/react-security';
+import { ManufactureDetail } from '../../context/ManufactureDetail';
 
 const EditManufactureAction = () => {
   const { t } = useTranslation('manufacture');
+  const { update } = useSearchParamsChange('edit');
+  const { manufacturerId } = ManufactureDetail();
+
+  const handleEdit = () => {
+    update({ edit: manufacturerId });
+  };
 
   return (
-    <Button variant='outlined' startIcon={<EditOutlinedIcon />}>
+    <Button variant='outlined' startIcon={<EditOutlinedIcon />} onClick={handleEdit}>
       {t('bulkActions.edit')}
     </Button>
   );
