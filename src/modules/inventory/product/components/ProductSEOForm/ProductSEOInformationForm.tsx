976275177a54a735524ffabf0e03ff42ed9845a9
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-23 13:56:48 -0500

    fixing product Basic information And product seo information

--- src/modules/inventory/product/components/ProductSEOForm/ProductSEOInformationForm.tsx
+++ src/modules/inventory/product/components/ProductSEOForm/ProductSEOInformationForm.tsx
@@ -1,7 +1,7 @@
 import { FormEventHandler, memo } from 'react';
-import { Form, FormTextField, HandlerError } from '@dfl/mui-react-common';
+import { Form, HandlerError } from '@dfl/mui-react-common';
 import { Grid } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import SeoForm from 'modules/inventory/product/containers/ProductFormSections/SeoForm';
 
 type ProductSEOInformationFormProps = {
   error: any;
@@ -11,31 +11,12 @@ type ProductSEOInformationFormProps = {
 };
 
 const ProductSEOInformationForm = ({ error, control, isLoading, onSubmit }: ProductSEOInformationFormProps) => {
-  const { t } = useTranslation('product');
-
   return (
     <div>
       <HandlerError error={error} />
       <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'}>
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          <Grid item xs={12} md={12}>
-            <FormTextField
-              fullWidth
-              helperText={t('section.searchPreview.labelTextHelper')}
-              name='seo.name'
-              label={t('section.searchPreview.labelTitle')}
-            />
-          </Grid>
-          <Grid item xs={12} md={12}>
-            <FormTextField
-              multiline
-              minRows={2}
-              fullWidth
-              name='seo.description'
-              helperText={t('section.searchPreview.descriptionTextHelper')}
-              label={t('section.searchPreview.labelDescription')}
-            />
-          </Grid>
+          <SeoForm />
         </Grid>
       </Form>
     </div>
