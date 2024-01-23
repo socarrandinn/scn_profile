129a61a3f52a7df930284b675ce458641b796ae9
Padin <padin790@gmail.com>
2024-01-23 12:55:34 -0500

    Delete a comment

--- src/modules/inventory/provider/supplier/pages/tabs/SupplierUsersPage.tsx
+++ src/modules/inventory/provider/supplier/pages/tabs/SupplierUsersPage.tsx
@@ -1,16 +1,14 @@
 import { memo } from 'react';
-import { PagePaperLayout } from 'layouts/index';
 import { useTranslation } from 'react-i18next';
 import { TableProvider } from '@dfl/mui-admin-layout';
-// import SupplierProductListContainer from '../../containers/SupplierProductListContainer';
+import { PagePaperLayout } from 'layouts/index';
 import SupplierUsersListContainer from '../../containers/SupplierUsersListContainer';
-// import { supplierProductTabFilters } from 'modules/inventory/product/constants';
 
 const SupplierUsersPage = () => {
   const { t } = useTranslation('users');
   return (
     <PagePaperLayout title={t('userList')} sx={{ mt: 0 }}>
-      <TableProvider id={'supplier-users'} >           {/* Faltan los filtros */}
+      <TableProvider id={'supplier-users'} >
         <SupplierUsersListContainer />
       </TableProvider>
     </PagePaperLayout>
