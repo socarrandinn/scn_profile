99a71ae31f8f760ecfb0fd511e0f64826abe38e5
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-24 15:17:21 -0500

    update role provider select role type tranlate

--- src/modules/security/roles/components/SelectRoleProviderType/SelectRoleProviderType.tsx
+++ src/modules/security/roles/components/SelectRoleProviderType/SelectRoleProviderType.tsx
@@ -2,6 +2,23 @@ import { FormFieldControlProps, FormSelectField } from '@dfl/mui-react-common';
 import { MenuItem, SelectProps } from '@mui/material';
 import { memo } from 'react';
 import { ROLE_PROVIDER_TYPE_ENUM } from '../../constants/role-provider.enum';
+import { useTranslation } from 'react-i18next';
+
+const getCustomLabel = (value: string): string => {
+  const { t } = useTranslation('role');
+  switch (value) {
+    case ROLE_PROVIDER_TYPE_ENUM.PRODUCT:
+      return t('roleProviderProduct');
+    case ROLE_PROVIDER_TYPE_ENUM.LOGISTIC:
+      return t('roleProviderLogistic');
+    case ROLE_PROVIDER_TYPE_ENUM.CARRIER:
+      return t('roleProviderCarrier');
+    case ROLE_PROVIDER_TYPE_ENUM.MANUFACTURER:
+      return t('roleProviderManufacturer');
+    default:
+      return value;
+  }
+};
 
 const SelectRoleProviderType = (props: FormFieldControlProps & SelectProps) => {
   return (
@@ -9,7 +26,7 @@ const SelectRoleProviderType = (props: FormFieldControlProps & SelectProps) => {
       {Object.entries(ROLE_PROVIDER_TYPE_ENUM).map(([value, label]) => {
         return (
           <MenuItem key={value} value={value}>
-            {label}
+            {getCustomLabel(value)}
           </MenuItem>
         );
       })}
