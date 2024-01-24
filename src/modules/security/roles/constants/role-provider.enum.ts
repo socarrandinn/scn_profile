f8e1e8259a95e0674587073e410e53d81ba2a1ba
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-24 15:17:21 -0500

    update role provider select role type tranlate

--- src/modules/security/roles/constants/role-provider.enum.ts
+++ src/modules/security/roles/constants/role-provider.enum.ts
@@ -1,6 +1,6 @@
 export enum ROLE_PROVIDER_TYPE_ENUM {
-  PRODUCT = 'Proveedor de Productos',
-  LOGISTIC = 'Proveedor Logistico',
-  CARRIER = 'Transportista',
-  MANUFACTURER = 'Fabricante',
+  PRODUCT = 'PRODUCT',
+  LOGISTIC = 'LOGISTIC',
+  CARRIER = 'CARRIER',
+  MANUFACTURER = 'MANUFACTURER',
 }
