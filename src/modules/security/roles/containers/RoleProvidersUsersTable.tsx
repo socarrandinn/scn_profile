6851236161cf6902fe33b21f49930df04b3bec1f
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-24 15:38:54 -0500

    add type filter to role provider list

--- src/modules/security/roles/containers/RoleProvidersUsersTable.tsx
+++ src/modules/security/roles/containers/RoleProvidersUsersTable.tsx
@@ -3,12 +3,13 @@ import { TableProvider } from '@dfl/mui-admin-layout';
 import { Paper } from '@mui/material';
 import { useRoleProviderDetail } from '../contexts/RoleProviderDetailContext';
 import { RoleProviderUsersList } from '../components/RoleProviderUsersList/RoleProviderUsersList';
+import { roleProviderListFilters } from '../constants/role-provider.filters';
 
 const RoleProviderUsersTable = () => {
   const { data, roleId } = useRoleProviderDetail();
   return (
         <Paper sx={{ marginBottom: 3, padding: 4 }}>
-            <TableProvider id={'role-provider-users'} >
+            <TableProvider id={'role-provider-users'} filters={roleProviderListFilters} >
                 <RoleProviderUsersList providerType={data?.type} roleId={roleId} />
             </TableProvider>
         </Paper>
