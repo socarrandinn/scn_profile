0a477978f0dbd18fd5ab33bd1a48defff4672f9b
NJ9802 <nelsonjavier9802@gmail.com>
2024-01-25 14:47:58 -0500

    Fix edit action.

--- src/modules/crm/clients/components/ClientsForm/ClientsForm.tsx
+++ src/modules/crm/clients/components/ClientsForm/ClientsForm.tsx
@@ -17,7 +17,7 @@ const ClientsForm = ({ error, control, isLoading, onSubmit }: ClientsFormProps) 
   return (
     <div>
       <HandlerError error={error} />
-      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'} dark>
+      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'ClientsForm'} dark>
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={12} md={6}>
             <FormTextField fullWidth autoFocus required name='firstName' label={t('fields.firstName')} />
--- src/modules/crm/clients/schemas/clients.schema.ts
+++ src/modules/crm/clients/schemas/clients.schema.ts
@@ -2,6 +2,9 @@ import * as Yup from 'yup';
 import '@dfl/yup-validations';
 
 export const clientsSchema = Yup.object().shape({
-  name: Yup.string().required('required').min(4, 'min-4').max(255, 'max-255'),
-  description: Yup.string().required('required').min(4, 'min-4'),
+  firstName: Yup.string().required('required').min(4, 'min-4').max(255, 'max-255'),
+  lastName: Yup.string().required('required').min(4, 'min-4').max(255, 'max-255'),
+  email: Yup.string().required('required').trim().email('validEmail').max(255),
+  // @ts-ignore
+  phone: Yup.string().phone('validPhone'),
 });
