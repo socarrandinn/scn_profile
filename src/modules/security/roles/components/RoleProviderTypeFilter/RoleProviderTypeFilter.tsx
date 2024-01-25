63fd16a3cf8678c1b6ea94f321f4d05782897c4d
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-25 15:34:10 -0500

    improving imports

--- src/modules/security/roles/components/RoleProviderTypeFilter/RoleProviderTypeFilter.tsx
+++ src/modules/security/roles/components/RoleProviderTypeFilter/RoleProviderTypeFilter.tsx
@@ -1,7 +1,7 @@
-import { FilterProps, FixedListFilter } from '@dfl/mui-admin-layout';
-import { map } from 'lodash';
-import { ROLE_PROVIDER_TYPE_ENUM } from 'modules/security/roles/constants/role-provider.enum';
 import { memo } from 'react';
+import { map } from 'lodash';
+import { FilterProps, FixedListFilter } from '@dfl/mui-admin-layout';
+import { ROLE_PROVIDER_TYPE_ENUM } from 'modules/security/roles/constants/role-provider.enum';
 import { getCustomLabel } from 'modules/security/roles/components/SelectRoleProviderType/SelectRoleProviderType';
 
 const RoleProviderTypeFilter = ({ filter, ...props }: FilterProps) => {
