dbf1e5c931d7532fca0980e44b54a4327d8a4dfd
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-26 14:44:21 -0500

    improving translate files

--- src/modules/inventory/store/components/StoreCubanMap/StoreCubanMap.tsx
+++ src/modules/inventory/store/components/StoreCubanMap/StoreCubanMap.tsx
@@ -6,9 +6,15 @@ import { useStoreDetail } from 'modules/inventory/store/context/StoreContext';
 export const StoreCubanMap = () => {
   const { t } = useTranslation('provider');
   const { store } = useStoreDetail();
+  let provincesCode: string[] = [];
+  if (Array.isArray(store?.address?.state)) {
+    provincesCode = store?.address?.state || [];
+  }
+  provincesCode.push(store?.address?.state || '');
+
   return (
-    <FormPaper title={t('fields.basicInformation')}>
-      <SvgCubanMap provinciasIds={store?.locations} />
+    <FormPaper title={t('mapLocation')}>
+      <SvgCubanMap provinciasIds={provincesCode} />
     </FormPaper>
   );
 };
