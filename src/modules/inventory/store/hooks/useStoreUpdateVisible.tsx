9d032578cea4f94b67506b441708fb994238e898
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-30 10:23:40 -0500

    improving inventory product lis

--- src/modules/inventory/store/hooks/useStoreUpdateVisible.tsx
+++ src/modules/inventory/store/hooks/useStoreUpdateVisible.tsx
@@ -2,7 +2,7 @@ import toast from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { StoreService } from 'modules/inventory/store/services';
-import { STORES_LIST_KEY } from 'modules/inventory/store/constants';
+import { STORES_LIST_KEY, STORES_PRODUCT_LIST_KEY } from 'modules/inventory/store/constants';
 
 const useStoreUpdateVisible = (storeId: string) => {
   const { t } = useTranslation(['store', 'errors']);
@@ -11,6 +11,7 @@ const useStoreUpdateVisible = (storeId: string) => {
   const { mutate, isLoading } = useMutation((visible: boolean) => StoreService.update(storeId, { visible }), {
     onSuccess: ({ data }: any) => {
       queryClient.invalidateQueries([STORES_LIST_KEY]);
+      queryClient.invalidateQueries([STORES_PRODUCT_LIST_KEY]);
       queryClient.invalidateQueries([storeId]);
       toast.success(t('successUpdate'));
     },
