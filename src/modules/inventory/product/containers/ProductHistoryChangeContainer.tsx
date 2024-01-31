e5fa531d0e0d1a13f81fd6f3a611c4fc94e13d1f
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-01-31 15:35:43 -0500

    delete history change header

--- src/modules/inventory/product/containers/ProductHistoryChangeContainer.tsx
+++ src/modules/inventory/product/containers/ProductHistoryChangeContainer.tsx
@@ -1,14 +1,10 @@
 import { Stack } from '@mui/material';
 import { memo } from 'react';
-import {
-  ProductHistoryChangeHeader,
-  ProductHistoryChangeInformation,
-} from 'modules/inventory/product/components/ProductHistoryChange';
+import { ProductHistoryChangeInformation } from 'modules/inventory/product/components/ProductHistoryChange';
 
 const ProductHistoryChangeContainer = () => {
   return (
     <Stack mb={{ xs: 2, md: 4 }} gap={{ xs: 1, md: 2 }}>
-      <ProductHistoryChangeHeader />
       <ProductHistoryChangeInformation />
     </Stack>
   );
