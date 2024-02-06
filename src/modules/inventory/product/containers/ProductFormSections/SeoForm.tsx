1536abe059f9a63087905ef2bd30032093d1b04d
Reynaldo <reybarcelo950@gmail.com>
2024-02-06 12:19:12 -0500

    [bugFix] improve seo form

--- src/modules/inventory/product/containers/ProductFormSections/SeoForm.tsx
+++ src/modules/inventory/product/containers/ProductFormSections/SeoForm.tsx
@@ -15,48 +15,40 @@ const SeoForm = ({ seoTitle, seoDescription }: SeoFormProps) => {
   const description = watch?.('seo.description') || seoDescription;
 
   return (
-    <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      <Grid item xs={12} md={12}>
-        <Small>{t('section.searchPreview.subtitle')}</Small>
-      </Grid>
-      <Grid item xs={12} md={12}>
-        <SeoPreview title={title} description={description} />
-      </Grid>
-      <Grid item xs={12} md={12}>
-        <FormTextField
-          fullWidth
-          helperText={t('section.searchPreview.labelTextHelper')}
-          name='seo.name'
-          label={t('section.searchPreview.labelTitle')}
-        />
-      </Grid>
-      <Grid item xs={12} md={12}>
-        <FormTextField
-          multiline
-          minRows={2}
-          fullWidth
-          name='seo.description'
-          helperText={t('section.searchPreview.descriptionTextHelper')}
-          label={t('section.searchPreview.labelDescription')}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <FormTextField
-          fullWidth
-          name='seo.slugUrl'
-          helperText={t('section.seo.slugHelperText')}
-          label={t('section.seo.slugUrl')}
-        />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <FormTextField
-          fullWidth
-          name='seo.canonicalURL'
-          helperText={t('section.seo.canonicalHelperText')}
-          label={t('section.seo.canonicalUrl')}
-        />
-      </Grid>
-    </Grid>
+        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <Grid item xs={12} md={12}>
+                <Small>{t('section.searchPreview.subtitle')}</Small>
+            </Grid>
+            <Grid item xs={12} md={12}>
+                <SeoPreview title={title} description={description}/>
+            </Grid>
+            <Grid item xs={12} md={12}>
+                <FormTextField
+                    fullWidth
+                    helperText={t('section.searchPreview.labelTextHelper')}
+                    name='seo.name'
+                    label={t('section.searchPreview.labelTitle')}
+                />
+            </Grid>
+            <Grid item xs={12} md={12}>
+                <FormTextField
+                    multiline
+                    minRows={2}
+                    fullWidth
+                    name='seo.description'
+                    helperText={t('section.searchPreview.descriptionTextHelper')}
+                    label={t('section.searchPreview.labelDescription')}
+                />
+            </Grid>
+            <Grid item xs={12} md={12}>
+                <FormTextField
+                    fullWidth
+                    name='seo.slug'
+                    helperText={t('section.seo.slugHelperText')}
+                    label={t('section.seo.slugUrl')}
+                />
+            </Grid>
+        </Grid>
   );
 };
 
