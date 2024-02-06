f2f5045f8587d77b1af810326699fe18521ec02d
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-06 11:01:40 -0500

    fixing order of product inventory table columns

--- src/modules/inventory/product/containers/ProductInventoryContainer.tsx
+++ src/modules/inventory/product/containers/ProductInventoryContainer.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react';
 import { Stack } from '@mui/material';
+import { TableProvider } from '@dfl/mui-admin-layout';
 import { ProductDetailProvider } from 'modules/inventory/product/contexts/ProductDetail';
 import ProductDetailInventoryListContainer from './ProductTabs/ProductDetailInventoryListContainer';
 import { FormPaper } from 'modules/common/components/FormPaper';
@@ -9,7 +10,9 @@ const SupplierInventoryContainer = () => {
     <ProductDetailProvider>
       <Stack mb={{ xs: 2, md: 4 }}>
         <FormPaper nm>
-          <ProductDetailInventoryListContainer />
+          <TableProvider id={'inventoryProduct'}>
+            <ProductDetailInventoryListContainer />
+          </TableProvider>
         </FormPaper>
       </Stack>
     </ProductDetailProvider>
