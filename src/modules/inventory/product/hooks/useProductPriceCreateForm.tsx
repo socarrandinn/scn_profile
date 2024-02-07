1961ec1ccff43c018ecd7985a6e41c2daef16d36
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-07 10:31:14 -0500

    fixing update of the faithful form discount ornament when the price is updated

--- src/modules/inventory/product/hooks/useProductPriceCreateForm.tsx
+++ src/modules/inventory/product/hooks/useProductPriceCreateForm.tsx
@@ -18,10 +18,15 @@ const initValues: Partial<IProductCreate> = {
 const useProductPriceCreateForm = (defaultValues: Partial<IProductCreate> = initValues) => {
   const { t } = useTranslation('provider');
   const queryClient = useQueryClient();
-  const { control, handleSubmit, reset, formState } = useForm({
+  const { control, handleSubmit, reset, formState, watch } = useForm({
     resolver: yupResolver(productPriceSchema),
     defaultValues,
   });
+
+  const { type: logisticPriceType } = watch?.('priceDetails.distribution.logistic');
+  const { type: shippingPriceType } = watch?.('priceDetails.distribution.shipping');
+  const { type: commercialPriceType } = watch?.('priceDetails.distribution.commercial');
+  const { type: otherCostPriceType } = watch?.('priceDetails.distribution.otherCost');
 
   useEffect(() => {
     // @ts-ignore
@@ -48,6 +53,10 @@ const useProductPriceCreateForm = (defaultValues: Partial<IProductCreate> = init
     isSuccess,
     data,
     reset,
+    logisticPriceType,
+    shippingPriceType,
+    commercialPriceType,
+    otherCostPriceType,
     values: formState.errors,
     // @ts-ignore
     onSubmit: handleSubmit((values) => {
