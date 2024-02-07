72628e8b281cb963d098c353a7ce45eb51b19e0d
Reynaldo <reybarcelo950@gmail.com>
2024-02-07 15:32:54 -0500

    [bugFix] improve visual user tab info

--- src/modules/inventory/store/components/UserStore/UserStore.tsx
+++ src/modules/inventory/store/components/UserStore/UserStore.tsx
@@ -1,12 +1,9 @@
 import { memo } from 'react';
 import { UserList } from 'modules/security/users/pages';
-import { PaperTabView } from 'modules/common/components/TabsWithSections/PaperTabView';
 
 const UserStore = () => {
   return (
-    <PaperTabView firsts>
-      <UserList />
-    </PaperTabView>
+        <UserList mt={0}/>
   )
 }
 
