3a1250f484d99e237e68566dbb21d2cd51514f06
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-08 14:30:40 -0500

    add decrease causes enum

---
+++ src/modules/inventory/product/constants/decrement-causes.enum.ts
@@ -0,0 +1,7 @@
+export enum DECREASE_CAUSES_TYPE_ENUM {
+  EXPIRATION = 'EXPIRATION', // vencimiento
+  LOSSES = 'LOSSES', // MERMA
+  INCIDENTS = 'INCIDENTS',
+  OTHERS = 'OTHERS', // incidencias
+  ATTENTION = 'ATTENTION', // atención a trabajadores
+}
