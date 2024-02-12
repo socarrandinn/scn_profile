0c441c5dc34223b905a3c08b9941de2e8e42faf1
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-11 22:59:22 -0500

    Add general estimated time section

---
+++ src/modules/inventory/product/components/ProductGeneralEstimatedTime/ProductGeneralEstimatedTime.tsx
@@ -0,0 +1,58 @@
+import { memo } from 'react';
+import { FormPaper } from 'modules/common/components/FormPaper';
+import { useTranslation } from 'react-i18next';
+import { useProductDetail } from 'modules/inventory/product/contexts/ProductDetail';
+import { simpleColumns } from 'modules/inventory/store/constants/store.simple.columns';
+import { BasicTableHeadless } from 'modules/common/components/BasicTableHeadless';
+import { useToggle } from '@dfl/hook-utils';
+import { FormPaperAction } from 'modules/common/components/FormPaperAction';
+import ProductDetailBasicUpdateContainer from 'modules/inventory/product/containers/ProductTabs/ProductDetailBasicUpdateContainer';
+import { IProduct } from 'modules/inventory/product/interfaces/IProduct';
+
+const ProductGeneralEstimatedTime = () => {
+  const { t } = useTranslation('product');
+  const { isOpen, onClose, onToggle } = useToggle(false);
+  const { isLoading, error, product } = useProductDetail();
+
+  if (isOpen) {
+    return (
+      <FormPaper
+        nm
+        title={t('section.deliveryTime.title')}
+        actions={<FormPaperAction onToggle={onToggle} open={isOpen} />}
+      >
+        <ProductDetailBasicUpdateContainer
+          initValue={{
+            _id: product?._id,
+            shippingSettings: product?.shippingSettings,
+          }}
+          dataError={error}
+          loadingInitData={isLoading}
+          onClose={onClose}
+        />
+      </FormPaper>
+    );
+  }
+
+  return (
+    <FormPaper nm title={t('section.deliveryTime.title')} actions={<FormPaperAction onToggle={onToggle} open={isOpen} />}>
+      <BasicTableHeadless
+        columns={simpleColumns}
+        data={getArray(product as IProduct) || []}
+        isLoading={isLoading}
+        error={error}
+      />
+    </FormPaper>
+  );
+};
+
+export default memo(ProductGeneralEstimatedTime);
+
+const getArray = (data: IProduct): any[] => {
+  const { shippingSettings } = data || {};
+  const array = [
+    { label: 'fields.labelMinMinutes', value: shippingSettings?.estimatedTime?.from },
+    { label: 'fields.labelMaxMinutes', value: shippingSettings?.estimatedTime?.to },
+  ];
+  return array;
+};
