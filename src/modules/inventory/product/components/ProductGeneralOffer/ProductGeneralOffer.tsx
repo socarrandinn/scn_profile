9b69e70263893786e6ed3472cfd12e860976746b
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-15 19:07:44 -0500

    add product general offer section

---
+++ src/modules/inventory/product/components/ProductGeneralOffer/ProductGeneralOffer.tsx
@@ -0,0 +1,56 @@
+import { memo } from 'react';
+import { FormPaper } from 'modules/common/components/FormPaper';
+import { useTranslation } from 'react-i18next';
+import { useProductDetail } from 'modules/inventory/product/contexts/ProductDetail';
+import { simpleColumns } from 'modules/inventory/store/constants/store.simple.columns';
+import { BasicTableHeadless } from 'modules/common/components/BasicTableHeadless';
+import { useToggle } from '@dfl/hook-utils';
+import { FormPaperAction } from 'modules/common/components/FormPaperAction';
+import ProductDetailBasicUpdateContainer from 'modules/inventory/product/containers/ProductTabs/ProductDetailBasicUpdateContainer';
+import { IProduct } from 'modules/inventory/product/interfaces/IProduct';
+
+const ProductGeneralOffer = () => {
+  const { t } = useTranslation('product');
+  const { isOpen, onClose, onToggle } = useToggle(false);
+  const { isLoading, error, product } = useProductDetail();
+
+  if (isOpen) {
+    return (
+      <FormPaper nm title={t('fields.generaldata')} actions={<FormPaperAction onToggle={onToggle} open={isOpen} />}>
+        <ProductDetailBasicUpdateContainer
+          initValue={{
+            _id: product?._id,
+            offer: product?.offer,
+          }}
+          dataError={error}
+          loadingInitData={isLoading}
+          onClose={onClose}
+        />
+      </FormPaper>
+    );
+  }
+
+  return (
+    <FormPaper nm title={t('fields.generaldata')} actions={<FormPaperAction onToggle={onToggle} open={isOpen} />}>
+      <BasicTableHeadless
+        columns={simpleColumns}
+        data={getArray(product as IProduct) || []}
+        isLoading={isLoading}
+        error={error}
+      />
+    </FormPaper>
+  );
+};
+
+export default memo(ProductGeneralOffer);
+
+const getArray = (data: IProduct): any[] => {
+  const { type, offer, from, to } = data.offer || {};
+  const array = [
+    { label: 'fields.name', value: type },
+    { label: 'fields.brand', value: offer },
+    { label: 'fields.code', value: from?.toString() },
+    { label: 'fields.barcode', value: to?.toString() },
+  ];
+  return array;
+};
