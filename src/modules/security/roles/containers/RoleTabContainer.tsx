e3d46892cbde94ce9c73c98d4e3d83bacee26671
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-26 09:47:59 -0500

    refactoring role tab container

--- src/modules/security/roles/containers/RoleTabContainer.tsx
+++ src/modules/security/roles/containers/RoleTabContainer.tsx
@@ -1,16 +1,17 @@
+import { Suspense, memo } from 'react';
 import PageTabPaperLayout from 'layouts/PageLayouts/PageTabPaperLayout';
-import { RouteLoader } from '@dfl/react-security';
-import tabActionRoutes from '../routes/tabActionRoutes';
-import { rolesTabs } from '../constants/tabs.details';
 import { PageLoader } from '@dfl/mui-react-common';
-import { Suspense } from 'react';
+import { RouteLoader } from '@dfl/react-security';
+import tabActionRoutes from 'modules/security/roles/routes/tabActionRoutes';
+import { rolesTabs } from 'modules/security/roles/constants/tabs.details';
 
-export default function RoleTabContianer () {
+const RoleTabContianer = () => {
   return (
     <PageTabPaperLayout prefix={'/security/roles'} tabs={rolesTabs}>
       <Suspense fallback={<PageLoader size={'screen'} />}>
-        <RouteLoader routes={tabActionRoutes} notfoundRedirect={'/security/roles/system'} />
+        <RouteLoader routes={tabActionRoutes} notfoundRedirect={'/security/roles/system'} memory />
       </Suspense>
     </PageTabPaperLayout>
   );
-}
+};
+export default memo(RoleTabContianer);
