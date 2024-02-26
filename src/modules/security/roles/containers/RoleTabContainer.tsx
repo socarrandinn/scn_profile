0ee825332cabb29bbdd4315a04efec3d5f3a0623
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-02-26 09:35:21 -0500

    add suspense to role tab container

--- src/modules/security/roles/containers/RoleTabContainer.tsx
+++ src/modules/security/roles/containers/RoleTabContainer.tsx
@@ -2,14 +2,15 @@ import PageTabPaperLayout from 'layouts/PageLayouts/PageTabPaperLayout';
 import { RouteLoader } from '@dfl/react-security';
 import tabActionRoutes from '../routes/tabActionRoutes';
 import { rolesTabs } from '../constants/tabs.details';
+import { PageLoader } from '@dfl/mui-react-common';
+import { Suspense } from 'react';
 
 export default function RoleTabContianer () {
   return (
     <PageTabPaperLayout prefix={'/security/roles'} tabs={rolesTabs}>
-    <RouteLoader
-        routes={tabActionRoutes}
-        notfoundRedirect={'/security/roles/system'}
-      />
+      <Suspense fallback={<PageLoader size={'screen'} />}>
+        <RouteLoader routes={tabActionRoutes} notfoundRedirect={'/security/roles/system'} />
+      </Suspense>
     </PageTabPaperLayout>
   );
 }
