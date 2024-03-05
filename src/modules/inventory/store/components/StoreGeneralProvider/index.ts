2fd5fafbb82c7fab67aad3da333c48ce653f15c9
NJ9802 <nelsonjavier9802@gmail.com>
2024-03-05 11:20:26 -0500

    Add Provider Section to store edition.

---
+++ src/modules/inventory/store/components/StoreGeneralProvider/index.ts
@@ -0,0 +1,1 @@
+export { default as StoreGeneralProvider } from './StoreGeneralProvider';
--- src/modules/inventory/store/containers/StoreGeneralContainer.tsx
+++ src/modules/inventory/store/containers/StoreGeneralContainer.tsx
@@ -1,15 +1,17 @@
 import { Stack } from '@mui/material';
 import { memo } from 'react';
-import { StoreGeneralBasic } from '../components/StoreGeneralBasic';
-import { StoreGeneralAddress } from '../components/StoreGeneralAddress';
-import { StoreGeneralContact } from '../components/StoreGeneralContact';
-import { StoreGeneralLocations } from '../components/StoreGeneralLocations';
+import { StoreGeneralBasic } from 'modules/inventory/store/components/StoreGeneralBasic';
+import { StoreGeneralAddress } from 'modules/inventory/store/components/StoreGeneralAddress';
+import { StoreGeneralContact } from 'modules/inventory/store/components/StoreGeneralContact';
+import { StoreGeneralLocations } from 'modules/inventory/store/components/StoreGeneralLocations';
+import { StoreGeneralProvider } from 'modules/inventory/store/components/StoreGeneralProvider';
 
 const StoreGeneralContainer = () => {
   return (
     <Stack mb={{ xs: 2, md: 4 }}>
       <StoreGeneralLocations />
       <StoreGeneralBasic />
+      <StoreGeneralProvider />
       <StoreGeneralAddress />
       <StoreGeneralContact />
     </Stack>
