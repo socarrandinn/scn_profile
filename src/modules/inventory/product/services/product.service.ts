791aafc1d5f51d1aafed6a97fbd0e89414c7dfc4
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-06 16:03:58 -0500

    fixing edit price update

--- src/modules/inventory/product/services/product.service.ts
+++ src/modules/inventory/product/services/product.service.ts
@@ -1,6 +1,7 @@
 import { ApiClientService, EntityApiService, RequestConfig } from '@dfl/react-security';
 import { IAddress } from 'modules/common/interfaces';
 import { IProduct } from 'modules/inventory/product/interfaces/IProduct';
+// import { IProductPriceDetails } from 'modules/inventory/product/interfaces/IProductPriceDetails';
 
 class ProductService extends EntityApiService<IProduct> {
   updateAddressInfo = (productId: string, params: IAddress, config?: RequestConfig): Promise<IAddress> => {
@@ -16,6 +17,12 @@ class ProductService extends EntityApiService<IProduct> {
   productCode = (code: string, store: string): any => {
     return ApiClientService.post(this.getPath(`/by-code/${code}`), {
       store,
+    });
+  };
+
+  updatePrice = (productId: string, priceProduct: any): any => {
+    return ApiClientService.patch(this.getPath(`/${productId}/price`), {
+      priceDetails: priceProduct,
     });
   };
 }
