26e4f5443c709f41d23eb2ed5b4c0edddec9c10a
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-06 11:12:20 -0500

    add releated product columns

---
+++ src/modules/inventory/product/constants/releated-product.colums.ts
@@ -0,0 +1,26 @@
+import { HeadCell } from '@dfl/mui-admin-layout';
+import {
+  categoryNameColumn,
+  createdAtProductColumn,
+  productBrandColumn,
+  productCodeColumn,
+  productCostPriceColumn,
+  productImageColumn,
+  productOnlyNameColumn,
+  productPriceColumn,
+  supplierNameColumn,
+  visibleProductColumn,
+} from './product.columns';
+
+export const productReletedColumns: HeadCell[] = [
+  productImageColumn,
+  productOnlyNameColumn,
+  productCodeColumn,
+  supplierNameColumn,
+  visibleProductColumn,
+  productBrandColumn,
+  productCostPriceColumn,
+  productPriceColumn,
+  categoryNameColumn,
+  createdAtProductColumn,
+];
