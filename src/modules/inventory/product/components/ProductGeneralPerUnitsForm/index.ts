cd2960250c116194a16ddcd331f865ddd5c39c0c
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-05 20:52:08 -0500

    delete product per units section

--- src/modules/inventory/product/components/ProductGeneralPerUnitsForm/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ProductGeneralPerUnitsForm } from './ProductGeneralPerUnitsForm';
-export { default as SelectMeasure } from './SelectMeasure';
-export { default as SelectTypeOfMeasure } from './SelectTypeOfMeasure';
