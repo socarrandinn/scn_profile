a7467d4b068ddc32477894d6b4caa04d79f0fac6
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-06 11:05:32 -0500

    add releated product tab component

---
+++ src/modules/inventory/product/components/ProductReleatedProductTabForm/ProductReleatedProductTabForm.tsx
@@ -0,0 +1,25 @@
+import { FormEventHandler, memo } from 'react';
+import { Form, HandlerError } from '@dfl/mui-react-common';
+import { Grid, Box } from '@mui/material';
+import RelatedProductsForm from 'modules/inventory/product/containers/ProductFormSections/RelatedProductsForm';
+
+type ProductReleatedProductTabFormProps = {
+  error: any;
+  control: any;
+  isLoading: boolean;
+  onSubmit: FormEventHandler | undefined;
+};
+
+const ProductReleatedProductTabForm = ({ error, control, isLoading, onSubmit }: ProductReleatedProductTabFormProps) => {
+  return (
+    <Box paddingLeft={3}>
+      <HandlerError error={error} />
+      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'}>
+        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+          <RelatedProductsForm />
+        </Grid>
+      </Form>
+    </Box>
+  );
+};
+export default memo(ProductReleatedProductTabForm);
