b71377ed9feeb3691e742d1eeb98fe20ec00db5b
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-05 20:51:07 -0500

    delete product offer section

--- src/modules/inventory/product/schemas/product-offer.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from 'yup';
-import '@dfl/yup-validations';
-
-export const productOfferSchema = Yup.object().shape({
-  type: Yup.string(),
-  offer: Yup.string(),
-  from: Yup.date(),
-  to: Yup.date(),
-});
