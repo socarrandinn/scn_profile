5c2cb2097d8f02c4f93bd6921b5cb0bbcb7d04c0
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-06 12:06:17 -0500

    showing list of related products

--- src/modules/inventory/product/components/ProductReleatedProductTabForm/ProductReleatedProductTabForm.tsx
+++ src/modules/inventory/product/components/ProductReleatedProductTabForm/ProductReleatedProductTabForm.tsx
@@ -1,7 +1,9 @@
 import { FormEventHandler, memo } from 'react';
 import { Form, HandlerError } from '@dfl/mui-react-common';
 import { Grid, Box } from '@mui/material';
+import { TableProvider } from '@dfl/mui-admin-layout';
 import RelatedProductsForm from 'modules/inventory/product/containers/ProductFormSections/RelatedProductsForm';
+import ReleatedProductListContainer from 'modules/inventory/product/containers/ProductTabs/ReleatedProductListContainer';
 
 type ProductReleatedProductTabFormProps = {
   error: any;
@@ -12,14 +14,21 @@ type ProductReleatedProductTabFormProps = {
 
 const ProductReleatedProductTabForm = ({ error, control, isLoading, onSubmit }: ProductReleatedProductTabFormProps) => {
   return (
-    <Box paddingLeft={3}>
-      <HandlerError error={error} />
-      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'}>
-        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          <RelatedProductsForm />
-        </Grid>
-      </Form>
-    </Box>
+    <>
+      <Box paddingLeft={2}>
+        <HandlerError error={error} />
+        <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form'}>
+          <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <RelatedProductsForm />
+          </Grid>
+        </Form>
+      </Box>
+      <Box marginTop={3}>
+        <TableProvider id={'releatedProduct'}>
+          <ReleatedProductListContainer />
+        </TableProvider>
+      </Box>
+    </>
   );
 };
 export default memo(ProductReleatedProductTabForm);
