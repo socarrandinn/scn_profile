55699268901014fc80cdd349be43df2bcfd02f0d
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-06 09:57:19 -0500

    add product related update container

---
+++ src/modules/inventory/product/components/ProductRelatedProduct/ProductRelatedProduct.tsx
@@ -0,0 +1,28 @@
+import { memo } from 'react';
+import { FormPaper } from 'modules/common/components/FormPaper';
+import { useTranslation } from 'react-i18next';
+import { useProductDetail } from 'modules/inventory/product/contexts/ProductDetail';
+import ProductDetailRelatedProductUpdateContainer from 'modules/inventory/product/containers/ProductTabs/ProductDetailRelatedProductUpdateContainer';
+
+const ProductRelatedProduct = () => {
+  const { t } = useTranslation('product');
+  const { isLoading, error, product } = useProductDetail();
+
+  return (
+    <FormPaper
+      nm
+      title={t('section.prices.information')}
+    >
+      <ProductDetailRelatedProductUpdateContainer
+        initValue={{
+          _id: product?._id,
+          priceDetails: product?.priceDetails,
+        }}
+        dataError={error}
+        loadingInitData={isLoading}
+      />
+    </FormPaper>
+  );
+};
+
+export default memo(ProductRelatedProduct);
