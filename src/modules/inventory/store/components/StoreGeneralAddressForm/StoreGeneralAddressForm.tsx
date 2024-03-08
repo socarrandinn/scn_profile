643d5e5d42880814fa9d5018b3e56dead4ba5068
NJ9802 <nelsonjavier9802@gmail.com>
2024-03-08 09:43:32 -0500

    Change size large in store details form.

--- src/modules/inventory/store/components/StoreGeneralAddressForm/StoreGeneralAddressForm.tsx
+++ src/modules/inventory/store/components/StoreGeneralAddressForm/StoreGeneralAddressForm.tsx
@@ -21,7 +21,7 @@ const StoreGeneralAddressForm = ({
   return (
     <div>
       <HandlerError error={error} />
-      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form-address'}>
+      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} id={'form-address'}>
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={12}>
             <AddressInput name='address' stateValue={state} hideZip />
--- src/modules/inventory/store/components/StoreGeneralContactForm/StoreGeneralContactForm.tsx
+++ src/modules/inventory/store/components/StoreGeneralContactForm/StoreGeneralContactForm.tsx
@@ -17,7 +17,7 @@ const StoreGeneralContactForm = ({ error, control, isLoading, onSubmit }: StoreG
   return (
     <div>
       <HandlerError error={error} />
-      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} size={'small'} id={'form-store-contact'}>
+      <Form onSubmit={onSubmit} control={control} isLoading={isLoading} id={'form-store-contact'}>
         <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={12}>
             <FormPhoneInputArray name={'contacts.phones'} label={t('fields.contact.phones')} required />
