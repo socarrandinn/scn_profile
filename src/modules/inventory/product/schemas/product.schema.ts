1a2ac34151b4669132fb6243289ebfd5be498b57
Daniel Vidaillet Rojas <dvidaillet90@gmail.com>
2024-03-21 09:09:13 -0500

    improving price yup schema

--- src/modules/inventory/product/schemas/product.schema.ts
+++ src/modules/inventory/product/schemas/product.schema.ts
@@ -5,8 +5,6 @@ import { productGeneralInfoSchema } from 'modules/inventory/product/schemas/prod
 import { productPriceSchema } from 'modules/inventory/product/schemas/product-price.schema';
 import { productOrganizationSchema } from 'modules/inventory/product/schemas/product-organization.schema';
 
-export const productSchema = productGeneralInfoSchema.concat(productPriceSchema).concat(productOrganizationSchema);
-
 export const productScoreSchema = Yup.object().shape({
   score: Yup.number().min(0),
 });
@@ -36,3 +34,8 @@ export const productEstimatedTimeSchema = Yup.object().shape({
     }),
   }),
 });
+
+export const productSchema = productGeneralInfoSchema
+  .concat(productPriceSchema)
+  .concat(productOrganizationSchema)
+  .concat(productEstimatedTimeSchema);
