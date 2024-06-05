179efa8ef1137f1bcf8493bd173e122caec972e2
socarrandin <socarrandin@gmail.com>
2024-06-05 11:11:48 -0400

    identity tuambia

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -7,6 +7,7 @@ import { MAIN_MENU } from 'settings/main.menu';
 import { LogoSidebar } from 'components/Logo/Logo';
 import { useProvider } from 'hooks/useProvider';
 import { LOGISTIC_MENU } from 'settings/logistic.menu';
+import { useSettings } from 'contexts/SettingsProvider';
 
 declare type AdminSidebarProps = ChildrenProps & {
   onClose: () => void;
@@ -15,12 +16,13 @@ declare type AdminSidebarProps = ChildrenProps & {
 
 const Sidebar = (props: AdminSidebarProps) => {
   const { pathname } = useLocation();
+  const { isDark } = useSettings()
   const { providerId } = useProvider();
   const sections = useMenu(providerId ? LOGISTIC_MENU : MAIN_MENU);
 
   return (
     <AdminSidebar {...props}>
-      <LogoSidebar />
+      <LogoSidebar {...(isDark ? { primary: '#FFF', secondary: '#bdbdbd' } : {})} />
 
       <Box sx={{ flexGrow: 1 }} className={'cursor-pointer dfl-sidebar-menu'}>
         {sections.map((section) => (
