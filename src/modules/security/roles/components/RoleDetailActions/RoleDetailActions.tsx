2c569c37b7133b37cf5fec628eb1a9b5d983dc8d
yasmaaf95 <yasmaaf95@gmail.com>
2024-07-19 17:08:06 -0400

    permissions tab

--- src/modules/security/roles/components/RoleDetailActions/RoleDetailActions.tsx
+++ src/modules/security/roles/components/RoleDetailActions/RoleDetailActions.tsx
@@ -4,15 +4,18 @@ import { useTranslation } from 'react-i18next';
 import RoleDetailEditModal from 'modules/security/roles/containers/RoleDetailEditModal';
 import { useToggle } from '@dfl/hook-utils';
 import { DeleteAction } from 'modules/security/roles/components/DeleteAction';
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 import { useDeleteRole } from 'modules/security/roles/hooks/useDeleteRole';
 import { useNavigate } from 'react-router-dom';
 import { useRoleDetail } from 'modules/security/roles/contexts';
+import useAddPermissionToRoleProviderForm from '../../hooks/useAddPermissionToRoleProviderForm';
 
 const RoleDetailActions = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { data: role } = useRoleDetail();
+  const location = useLocation();
+  const permissionsTab = location.pathname.endsWith('/permissions');
+  const { data: role, permissions } = useRoleDetail();
 
   const { t } = useTranslation('role');
   const { isOpen, onOpen, onClose } = useToggle(false);
@@ -23,7 +26,7 @@ const RoleDetailActions = () => {
   };
 
   const { mutate } = useDeleteRole(id || '', onDelete);
-
+  const { mutate: addPermission } = useAddPermissionToRoleProviderForm(role, onClose);
   return (
     <>
       <Stack gap={2} flexDirection={'row'} flexWrap={'wrap'} alignItems={'start'}>
@@ -33,6 +36,18 @@ const RoleDetailActions = () => {
         <Button variant={'outlined'} color={'error'} onClick={handleOnOpenDelete} disabled={role?.isSystemRole}>
           {t('delete')}
         </Button>
+
+        {permissionsTab && (
+          <Button
+            variant={'contained'}
+            color={'primary'}
+            onClick={() => {
+              addPermission(permissions);
+            }}
+          >
+            {t('savePermissions')}
+          </Button>
+        )}
       </Stack>
       <RoleDetailEditModal isOpen={isOpen} onClose={onClose} />
       <DeleteAction open={isOpenDelete} onClose={handleOnCloseDelete} onDelete={mutate} />
