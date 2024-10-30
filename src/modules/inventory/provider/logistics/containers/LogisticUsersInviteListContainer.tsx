b8cdf4ee9df066008778f2e9fa084382fec8b60c
socarrandin <socarrandin@gmail.com>
2024-10-30 17:50:44 -0400

    [user-inivte] providers

--- src/modules/inventory/provider/logistics/containers/LogisticUsersInviteListContainer.tsx
+++ src/modules/inventory/provider/logistics/containers/LogisticUsersInviteListContainer.tsx
@@ -1,15 +1,15 @@
 import { memo } from 'react';
 import { useFindProviderUsersInvites } from 'modules/security/users-invite/hooks/useFindUsersInvites';
 import { useLogisticsDetailContext } from '../context/LogisticDetail';
-import { ProviderUsersInviteToolbar } from '../../../../security/users-invite/components/ProviderUsersInviteToolbar';
 import UsersInviteListContainer from 'modules/security/users-invite/containers/UsersInviteListContainer';
-import { LOGISTICS_PERMISSIONS } from '../constants';
+
+import { LogisticUsersToolbar } from '../components/LogisticUsersToolbar';
 
 const LogisticUsersInviteListContainer = () => {
   const { logisticId } = useLogisticsDetailContext();
   return (
     <UsersInviteListContainer
-      Toolbar={<ProviderUsersInviteToolbar permissions={[LOGISTICS_PERMISSIONS.LOGISTICS_WRITE]} />}
+      Toolbar={<LogisticUsersToolbar />}
       useHook={useFindProviderUsersInvites}
       entityId={logisticId}
     />
