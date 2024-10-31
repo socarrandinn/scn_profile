8d908e22304229dc6569510c681edf9c74dea9f6
socarrandin <socarrandin@gmail.com>
2024-10-31 10:58:15 -0400

    dc

--- src/modules/inventory/product/hooks/useFindProductByDistributionCenters.tsx
+++ src/modules/inventory/product/hooks/useFindProductByDistributionCenters.tsx
@@ -1,21 +1,16 @@
-import { useMemo } from 'react';
-import { TermFilter } from '@dofleini/query-builder';
 import { useTableRequest } from '@dfl/mui-admin-layout';
 import { useQuery } from '@tanstack/react-query';
-import { ProductService } from 'modules/inventory/product/services';
+import { DISTRIBUTION_CENTER_PRODUCT_LIST_KEY } from 'modules/inventory/distribution-centers/constants';
 import { useDistributionCenterDetail } from 'modules/inventory/distribution-centers/context/DistributioncentersContext';
-import { PRODUCTS_DISTRIBUTION_CENTER_LIST_KEY } from '../constants/query-keys';
+import { DistributionCentersService } from 'modules/inventory/distribution-centers/services';
 
 export const useFindProductByDistributionCenters = () => {
   const { distributionCenterId } = useDistributionCenterDetail();
 
-  const filter = useMemo(() => {
-    const storeFilter = { field: 'stock.distributionCenters', value: distributionCenterId };
-    return new TermFilter(storeFilter);
-  }, [distributionCenterId]);
-
-  const { fetch, queryKey, filters } = useTableRequest(ProductService.search, filter);
-  const query = useQuery([PRODUCTS_DISTRIBUTION_CENTER_LIST_KEY, queryKey], fetch, {
+  const { fetch, queryKey, filters } = useTableRequest((params, config) =>
+    DistributionCentersService.searchProducts(distributionCenterId, params, config),
+  );
+  const query = useQuery([DISTRIBUTION_CENTER_PRODUCT_LIST_KEY, queryKey], fetch, {
     enabled: !!distributionCenterId,
   });
 
