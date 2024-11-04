a7acf175907567b07ba94e4e7ecce278f10b5b5e
socarrandin <socarrandin@gmail.com>
2024-11-04 11:58:14 -0500

    add dc section to warehouse

---
+++ src/modules/inventory/warehouse/components/WarehouseDistributionCenter/WarehouseDistributionCenterSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Grid, Skeleton } from '@mui/material';
+import { memo } from 'react';
+
+const WarehouseDistributionCenterSkeleton = () => {
+  return (
+    <Grid container spacing={1}>
+      {Array(5)
+        .fill('')
+        .map((sk) => (
+          <Grid key={sk} item xs={12} md={6}>
+            <Skeleton sx={{ width: '100%', height: 35, }} variant='rounded' animation='wave' />
+          </Grid>
+        ))}
+    </Grid>
+  );
+};
+
+export default memo(WarehouseDistributionCenterSkeleton);
---
+++ src/modules/inventory/warehouse/components/WarehouseDistributionCenter/index.ts
@@ -0,0 +1,1 @@
+export { default as WarehouseDistributionCenter } from './WarehouseDistributionCenter';
