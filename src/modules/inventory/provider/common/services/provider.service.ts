4207e09cf3f32376ffe97fd0062112554bb98434
Keren <kerensanchezpadron@gmail.com>
2024-11-12 09:11:12 -0500

    [fix]: lint error in provider service

--- src/modules/inventory/provider/common/services/provider.service.ts
+++ src/modules/inventory/provider/common/services/provider.service.ts
@@ -1,13 +1,13 @@
-import { ApiClientService, EntityApiService } from '@dfl/react-security';
+import { ApiClientService, EntityApiService, RequestConfig } from '@dfl/react-security';
 import { IProvider } from 'modules/security/roles/interfaces/IProvider';
 
 class ProviderService extends EntityApiService<IProvider> {
-  searchProvidersByType = (params?: any, config?: any) => {
+  searchProvidersByType = (params: string, config?: RequestConfig) => {
     return this.handleResponse(ApiClientService.post(this.getPath(`/${params}/search`), params, config));
   };
 
-  getOneByType = (id: string, params?: any) => {
-    return this.handleResponse(ApiClientService.get(this.getPath(`/${params}/${id}`), params));
+  getOneByType = (id: string, params: string) => {
+    return this.handleResponse(ApiClientService.get(this.getPath(`/${params}/${id}`)));
   };
 }
 
