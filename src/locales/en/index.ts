6200a4bd3babcb13b2a2833f70529f460d971365
Keren <kerensanchezpadron@gmail.com>
2024-11-12 07:57:26 -0500

    [feat]: add english translations in account, audit log, authentication, breadcrumb, category, cause cancellation, causes incidence, client, common, coupon order, crm, distribution center, drop zone, errors, general settings, logistics, menu, order, pre order, product, product, product discount, products, product upload, provider, provider analytic, rate, report, report cause, reviews, role, seo, stock reduction cause, supplier, tags, users, users invite, warehouse area and warehouse pickup

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -4,7 +4,6 @@ export { default as account } from './account.json';
 export { default as errors } from './errors.json';
 export { default as locales } from './locales.json';
 export { default as users } from './users.json';
-export { default as results } from './results.json';
 export { default as role } from './role.json';
 export { default as menu } from './menu.json';
 export { default as trace } from './trace.json';
@@ -12,21 +11,21 @@ export { default as phoneTypes } from './phoneTypes.json';
 export { default as emailTypes } from './emailTypes.json';
 export { default as breadcrumb } from './breadcrumb.json';
 export { default as warehouse } from './warehouse.json';
-export { default as warehouseArea } from './warehouseArea.json';
+export { default as results } from './results.json';
 export { default as category } from './category.json';
+export { default as warehouseArea } from './warehouseArea.json';
 export { default as message } from './message.json';
 export { default as provider } from './provider.json';
 export { default as providerAnalytic } from './providerAnalytic.json';
 export { default as manufacture } from './manufacture.json';
 export { default as product } from './product.json';
 export { default as logistics } from './logistics.json';
-export { default as products } from './products.json';
 export { default as supplier } from './supplier.json';
-
 export { default as orderStatus } from './orderStatus.json';
 
 export { default as crm } from './crm.json';
 export { default as clients } from './clients.json';
+export { default as sales } from './sales.json';
 export { default as dashboard } from './dashboard.json';
 export { default as report } from './report.json';
 export { default as auditLog } from './auditLog.json';
@@ -34,12 +33,13 @@ export { default as warehousePickup } from './warehousePickup.json';
 
 export { default as causesIncidence } from './causesIncidence.json';
 export { default as causeCancellation } from './causeCancellation.json';
-export { default as sales } from './sales.json';
 
 export { default as homeDelivery } from './homeDelivery.json';
 
 export { default as expressDelivery } from './expressDelivery.json';
+
 export { default as notification } from './notification.json';
+export { default as rate } from './rate.json';
 
 export { default as reviews } from './reviews.json';
 export { default as offers } from './offers.json';
@@ -53,10 +53,15 @@ export { default as productDiscount } from './productDiscount.json';
 export { default as tags } from './tags.json';
 
 export { default as stockReductionCause } from './stockReductionCause.json';
+
 export { default as offerOrder } from './offerOrder.json';
 export { default as couponOrder } from './couponOrder.json';
+export { default as productUpload } from './productUpload.json';
 
 export { default as paidOrder } from './paidOrder.json';
 export { default as preOrder } from './preOrder.json';
 export { default as order } from './order.json';
 export { default as distributionCenters } from './distributionCenters.json';
+
+export { default as stock } from './stock.json';
+export { default as dropZone } from './dropZone.json';
