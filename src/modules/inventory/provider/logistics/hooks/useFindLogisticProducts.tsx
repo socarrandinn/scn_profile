a94f0dacce0fcb8f7997d59bca3037b445c19a5a
socarrandin <socarrandin@gmail.com>
2024-11-18 14:07:26 -0500

    [product] logistic and supplier

--- src/modules/inventory/provider/logistics/hooks/useFindLogisticProducts.tsx
+++ src/modules/inventory/provider/logistics/hooks/useFindLogisticProducts.tsx
@@ -1,18 +1,14 @@
-import { useMemo } from 'react';
-import { InFilter } from '@dofleini/query-builder';
 import { useTableRequest } from '@dfl/mui-admin-layout';
 import { useQuery } from '@tanstack/react-query';
-import { ProductService } from 'modules/inventory/product/services';
 import { LOGISTIC_PRODUCTS_LIST_KEY } from '../constants';
+import { LogisticsService } from '../services';
 
-export const useFindLogisticProducts = (logisticProviderId: string | undefined) => {
-  const filter = useMemo(() => {
-    return new InFilter({ field: 'warehouse.logistic', objectId: true, value: [logisticProviderId] });
-  }, [logisticProviderId]);
-
-  const { fetch, queryKey, filters } = useTableRequest(ProductService.search, filter);
-  const query = useQuery([LOGISTIC_PRODUCTS_LIST_KEY, queryKey], fetch, {
-    enabled: !!logisticProviderId,
+export const useFindLogisticProducts = (logisticId: string | undefined) => {
+  const { fetch, queryKey, filters } = useTableRequest((params, config) =>
+    LogisticsService.productSearch({ ...params, logisticId }, config),
+  );
+  const query = useQuery([LOGISTIC_PRODUCTS_LIST_KEY, queryKey, logisticId], fetch, {
+    enabled: !!logisticId,
   });
 
   return {
--- src/modules/inventory/provider/supplier/hooks/useFindSupplierProducts.ts
+++ src/modules/inventory/provider/supplier/hooks/useFindSupplierProducts.ts
@@ -1,19 +1,14 @@
-import { useMemo } from 'react';
-import { TermFilter } from '@dofleini/query-builder';
 import { useTableRequest } from '@dfl/mui-admin-layout';
 import { useQuery } from '@tanstack/react-query';
-// import { PROVIDER_PRODUCT_LIST_KEY } from '../../common/constants/querys';
-import { ProductService } from 'modules/inventory/product/services';
 import { PRODUCTS_LIST_KEY } from 'modules/inventory/product/constants';
+import { SupplierService } from '../services';
 
-export const useFindSupplierProducts = (providerProductId: string | undefined) => {
-  const filter = useMemo(() => {
-    return new TermFilter({ field: 'providers.supplier._id', value: providerProductId });
-  }, [providerProductId]);
-
-  const { fetch, queryKey, filters } = useTableRequest(ProductService.search, filter);
-  const query = useQuery([PRODUCTS_LIST_KEY, queryKey], fetch, {
-    enabled: !!providerProductId,
+export const useFindSupplierProducts = (supplierId: string | undefined) => {
+  const { fetch, queryKey, filters } = useTableRequest((params, config) =>
+    SupplierService.productSearch({ ...params, supplierId }, config),
+  );
+  const query = useQuery([PRODUCTS_LIST_KEY, queryKey, supplierId], fetch, {
+    enabled: !!supplierId,
   });
 
   return {
