afb5f9b9aaad830ca788361ddb0198752d81044c
Keren <kerensanchezpadron@gmail.com>
2024-11-25 13:36:31 -0500

    [fix]:  translation in product inventory status

--- src/modules/inventory/warehouse/components/StoreVisiblePicker/StoreVisiblePicker.tsx
+++ src/modules/inventory/warehouse/components/StoreVisiblePicker/StoreVisiblePicker.tsx
@@ -2,6 +2,7 @@ import { IStatus, StatusPicker } from '@dfl/mui-react-common';
 import { memo } from 'react';
 import { CATEGORY_VISIBILITY, CATEGORY_VISIBILITY_MAP } from 'modules/inventory/settings/category/constants';
 import useStoreUpdateVisible from 'modules/inventory/warehouse/hooks/useStoreUpdateVisible';
+import { useTranslation } from 'react-i18next';
 
 type CategoryVisiblePickerProps = {
   value: boolean;
@@ -9,13 +10,14 @@ type CategoryVisiblePickerProps = {
 };
 
 const StoreVisiblePicker = ({ value, rowId }: CategoryVisiblePickerProps) => {
+  const { t } = useTranslation('warehouse');
   const { updateVisible, isLoading } = useStoreUpdateVisible(rowId);
 
   return (
     <StatusPicker
-      options={CATEGORY_VISIBILITY}
+      options={CATEGORY_VISIBILITY.map((option) => ({ ...option, title: t(option.title) }))}
       name='active'
-      value={CATEGORY_VISIBILITY_MAP.get(value) as IStatus}
+      value={{ ...CATEGORY_VISIBILITY_MAP.get(value) as IStatus, title: t(CATEGORY_VISIBILITY_MAP.get(value)?.title as string) }}
       isLoading={isLoading}
       onChange={() => {
         updateVisible(!value);
