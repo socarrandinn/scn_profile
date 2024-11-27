6540e8cfe97eab9bc37108a52d7852135bf4556a
socarrandin <socarrandin@gmail.com>
2024-11-27 16:08:43 -0500

    fix relation add

--- src/modules/inventory/product-stock/components/ErrorContents/SupplierNoRelation/hooks/useSupplierNotRelationContext.tsx
+++ src/modules/inventory/product-stock/components/ErrorContents/SupplierNoRelation/hooks/useSupplierNotRelationContext.tsx
@@ -1,3 +1,4 @@
+import { IWarehouseSupplierNoExist } from 'modules/inventory/product-stock/interfaces/IStockSummary';
 import { create } from 'zustand';
 
 type IRelation = {
@@ -6,6 +7,7 @@ type IRelation = {
 };
 type State = {
   setRelationSupplier: (item: IRelation) => void;
+  hazRelationSupplier: (item: IWarehouseSupplierNoExist) => boolean;
   selected: IRelation[];
 };
 
@@ -16,11 +18,8 @@ export const useSupplierRelationContext = create<State>((set, get) => ({
     const { selected } = get();
     set({ selected: [...selected, item] });
   },
-
-  removeRelationSupplier: (item: IRelation) => {
+  hazRelationSupplier: (item: IWarehouseSupplierNoExist) => {
     const { selected } = get();
-    set({
-      selected: selected.filter((rel) => rel.supplier !== item.supplier || rel.warehouse !== item.warehouse),
-    });
+    return selected.some((rel) => rel.supplier === item.supplier.supplierId && rel.warehouse === item.warehouse.warehouseId);
   },
 }));
