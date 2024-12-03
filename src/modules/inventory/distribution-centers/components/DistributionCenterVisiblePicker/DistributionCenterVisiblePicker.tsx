4c337a9d83b58ac0efab393d1adc01c6bc742723
socarrandin <socarrandin@gmail.com>
2024-12-03 16:19:41 -0500

    fix visibility

--- src/modules/inventory/distribution-centers/components/DistributionCenterVisiblePicker/DistributionCenterVisiblePicker.tsx
+++ src/modules/inventory/distribution-centers/components/DistributionCenterVisiblePicker/DistributionCenterVisiblePicker.tsx
@@ -1,8 +1,9 @@
 import { IStatus, StatusPicker } from '@dfl/mui-react-common';
 import { memo } from 'react';
-import { CATEGORY_VISIBILITY, CATEGORY_VISIBILITY_MAP } from 'modules/inventory/settings/category/constants';
+import { CATEGORY_VISIBILITY } from 'modules/inventory/settings/category/constants';
 import useDistributionCenterUpdateVisible from '../../hooks/useDistributionCenterUpdateVisible';
 import { useTranslation } from 'react-i18next';
+import { useVisibilityStatus } from 'modules/inventory/common/hooks/useVisibilityStatus';
 
 type CategoryVisiblePickerProps = {
   value: boolean;
@@ -11,20 +12,17 @@ type CategoryVisiblePickerProps = {
 
 const DistributionCenterVisiblePicker = ({ value, rowId }: CategoryVisiblePickerProps) => {
   const { t } = useTranslation('category');
-  const { updateVisible, isLoading } = useDistributionCenterUpdateVisible(rowId);
-
+  const { updateVisible, isLoading, value: lastValue } = useDistributionCenterUpdateVisible(rowId);
+  const _value = useVisibilityStatus(value, lastValue);
   return (
     <StatusPicker
       options={CATEGORY_VISIBILITY.map((option) => ({ ...option, title: t(option.title) }))}
-      name='active'
+      name='visible'
+      value={_value}
       size={'small'}
-      value={{
-        ...(CATEGORY_VISIBILITY_MAP.get(value) as IStatus),
-        title: t(CATEGORY_VISIBILITY_MAP.get(value)?.title as string),
-      }}
       isLoading={isLoading}
-      onChange={() => {
-        updateVisible(!value);
+      onChange={(status: IStatus) => {
+        updateVisible(status?._id === 'true');
       }}
     />
   );
