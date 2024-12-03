5ef74804082566e4909fe183ffa8699aea8eb27c
socarrandin <socarrandin@gmail.com>
2024-12-03 12:22:00 -0500

    [export] product logisitc

---
+++ src/modules/export/components/modules/inventory/LogisticProductExportButton/index.ts
@@ -0,0 +1,1 @@
+export { default as LogisticProductExportButton } from './LogisticProductExportButton';
--- src/modules/logistic/containers/LogisticProductListContainer.tsx
+++ src/modules/logistic/containers/LogisticProductListContainer.tsx
@@ -4,7 +4,6 @@ import Box from '@mui/material/Box';
 import { supplierProductTabColumns } from 'modules/inventory/product/constants';
 import { useProvider } from 'hooks/useProvider';
 import { useFindLogisticProducts } from 'modules/inventory/provider/logistics/hooks/useFindLogisticProducts';
-import LogisticProductsToolbar from 'modules/inventory/provider/logistics/components/LogisticProductsToolbar/LogisticProductsToolbar';
 
 const LogisticProductListContainer = () => {
   const { providerId } = useProvider();
@@ -12,7 +11,6 @@ const LogisticProductListContainer = () => {
 
   return (
     <Box>
-      <LogisticProductsToolbar />
       <Table
         columns={supplierProductTabColumns}
         data={data?.data}
