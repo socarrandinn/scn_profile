7d0a8f426cc0c79664896bfd3e4e153f19b36d2b
socarrandin <socarrandin@gmail.com>
2024-12-11 15:04:12 -0500

    fix tag date

--- src/modules/inventory/settings/tags/components/TagsContentForm/TagItem/TagItemString.tsx
+++ src/modules/inventory/settings/tags/components/TagsContentForm/TagItem/TagItemString.tsx
@@ -3,6 +3,7 @@ import ProductTagLayout from './TagLayout';
 import { ISummaryTags } from 'modules/inventory/settings/tags/interfaces';
 import { Chip, SxProps } from '@mui/material';
 import { LongText } from 'modules/common/components/LongText';
+import { isArray } from 'lodash';
 
 type TagItemStringProps = {
   tag: ISummaryTags;
@@ -11,7 +12,7 @@ type TagItemStringProps = {
 };
 
 const TagItemString = ({ tag, sx, Component }: TagItemStringProps) => {
-  const label = Component ? <Component value={tag?.value} /> : tag?.value;
+  const label = Component ? <Component value={isArray(tag?.value) ? tag?.value?.[0] : tag?.value} /> : tag?.value;
   return (
     <ProductTagLayout title={tag?.name}>
       <Chip variant='outlined' label={<LongText text={label} lineClamp={1} />} sx={sx} />
