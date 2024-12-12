53c9fe53e02ea47108af9e038a9057ee3c14d0dd
Keren <kerensanchezpadron@gmail.com>
2024-12-12 11:31:31 -0500

    [feat]: add helper text in tags list

--- src/modules/inventory/settings/tags/pages/TagsList.tsx
+++ src/modules/inventory/settings/tags/pages/TagsList.tsx
@@ -1,19 +1,23 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { PagePaperLayout } from 'layouts/index';
+import { PageLayout, PagePaperLayout } from 'layouts/index';
 import { TableProvider } from '@dfl/mui-admin-layout';
 import TagsListContainer from 'modules/inventory/settings/tags/containers/TagsListContainer';
 import { tagsFilters } from 'modules/inventory/settings/tags/constants/tags.filters';
+import { HelperText } from '../../warehouse-area/components/HelperText';
 
 const TagsList = () => {
   const { t } = useTranslation('tags');
 
   return (
-    <PagePaperLayout title={t('list')}>
-      <TableProvider id={'tags'} filters={tagsFilters}>
-        <TagsListContainer />
-      </TableProvider>
-    </PagePaperLayout>
+    <PageLayout>
+      <HelperText text={t('description')} />
+      <PagePaperLayout title={t('list')}>
+        <TableProvider id={'tags'} filters={tagsFilters}>
+          <TagsListContainer />
+        </TableProvider>
+      </PagePaperLayout>
+    </PageLayout>
   );
 };
 
