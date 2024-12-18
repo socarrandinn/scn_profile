391fa7575afb4fdf8f9e08e8e418bae00ccb05a4
socarrandin <socarrandin@gmail.com>
2024-12-18 10:53:48 -0500

    [client] fix client Recipient

--- src/modules/crm/clients/hooks/useFindClients.ts
+++ src/modules/crm/clients/hooks/useFindClients.ts
@@ -1,10 +1,17 @@
+import { useMemo } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useTableRequest } from '@dfl/mui-admin-layout';
 import { ClientsService } from 'modules/crm/clients/services';
-import { CLIENTS_LIST_KEY } from 'modules/crm/clients/constants';
+import { CLIENTS_LIST_KEY, CLIENTS_RECIPIENT_LIST_KEY } from 'modules/crm/clients/constants';
+import { TermFilter } from '@dofleini/query-builder';
 
 export const useFindClients = () => {
   const { fetch, queryKey } = useTableRequest(ClientsService.search);
-
   return useQuery([CLIENTS_LIST_KEY, queryKey], fetch);
+};
+export const useFindRecipientsClients = (clientId: string) => {
+  const filters = useMemo(() => new TermFilter({ field: 'owner', value: clientId }), [clientId]);
+  const { fetch, queryKey } = useTableRequest(ClientsService.search, filters);
+
+  return useQuery([CLIENTS_RECIPIENT_LIST_KEY, queryKey], fetch, { enabled: !!clientId });
 };
