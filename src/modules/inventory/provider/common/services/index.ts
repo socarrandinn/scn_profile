1cad1f398101718392231375f219ade3bf3b9303
Keren <kerensanchezpadron@gmail.com>
2024-12-30 01:38:53 -0500

    [feat]: to allow update avatar in logistic provider and code refactor

--- src/modules/inventory/provider/common/services/index.ts
+++ src/modules/inventory/provider/common/services/index.ts
@@ -1,1 +1,2 @@
 export { default as ProviderService } from './provider.service';
+export { default as ProvidersUploadService } from './providers-upload.service';
--- src/modules/inventory/provider/logistics/services/index.ts
+++ src/modules/inventory/provider/logistics/services/index.ts
@@ -1,2 +1,1 @@
 export { default as LogisticsService } from './logistics.service';
-export { default as LogisticsUploadService } from './providerLogistics-upload.services';
