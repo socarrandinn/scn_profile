4a18d736cfcff4bb4fd6a656c2e3bd0caf46b7b7
Keren <kerensanchezpadron@gmail.com>
2025-01-05 01:51:46 -0500

    [feat]: spell check

--- src/modules/inventory/provider/supplier/components/UploadImgaEditSupplier/ImageSupplier.tsx
+++ src/modules/inventory/provider/supplier/components/UploadImgaEditSupplier/ImageSupplier.tsx
@@ -1,16 +1,16 @@
 import { memo } from 'react';
 import AvatarEditable from 'components/AvatarEditable/AvatarEditable';
 import NoFoodIcon from '@mui/icons-material/NoFood';
-import { useUploadImageProvedorProducts } from 'modules/inventory/provider/supplier/hooks/useUploadImageProvedorProducts';
 import { ISupplier } from 'modules/inventory/provider/supplier/interfaces';
+import { useUploadImageProviderProducts } from '../../hooks/useUploadImageProviderProducts';
 
 type ImageCategoryProps = {
-  provedorProducts?: ISupplier;
+  providerProducts?: ISupplier;
   size?: number
 };
 
-const ImageSupplier = ({ provedorProducts, size = 150 }: ImageCategoryProps) => {
-  const { mutate, isLoading } = useUploadImageProvedorProducts(provedorProducts?._id as string);
+const ImageSupplier = ({ providerProducts, size = 150 }: ImageCategoryProps) => {
+  const { mutate, isLoading } = useUploadImageProviderProducts(providerProducts?._id as string);
 
   const onSubmit = (f: any) => {
     if (f.length) {
@@ -19,12 +19,12 @@ const ImageSupplier = ({ provedorProducts, size = 150 }: ImageCategoryProps) => 
   }
 
   return (
-        <div>
-            <AvatarEditable avatar={provedorProducts?.avatar} onSubmit={onSubmit} isLoading={isLoading} size={size}
-                            variant={'rounded'}>
-                <NoFoodIcon fontSize='small'/>
-            </AvatarEditable>
-        </div>
+    <div>
+      <AvatarEditable avatar={providerProducts?.avatar} onSubmit={onSubmit} isLoading={isLoading} size={size}
+        variant={'rounded'}>
+        <NoFoodIcon fontSize='small' />
+      </AvatarEditable>
+    </div>
   );
 };
 
