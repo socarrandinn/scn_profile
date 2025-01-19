66db0960a9125ba2b6842db643bdf6e0928334ad
Keren <kerensanchezpadron@gmail.com>
2025-01-19 13:11:40 -0500

    [fix]: lint errors

--- src/modules/security/users/pages/UserList.tsx
+++ src/modules/security/users/pages/UserList.tsx
@@ -2,13 +2,12 @@ import { memo, useMemo } from 'react';
 import { TableProvider } from '@dfl/mui-admin-layout';
 import UserTableContainer, { UserTableContainerProps } from '../containers/UserTableContainer';
 import { userFilters } from '../constants/user-filters';
-import { SPACE_TYPE } from '../constants/space-types.constants';
 
 const UserList = ({ type, status }: UserTableContainerProps) => {
-  const filters = useMemo(() => userFilters(type as SPACE_TYPE), [type]);
+  const filters = useMemo(() => userFilters(type), [type]);
 
   return (
-    <TableProvider id={`user-list-${type}`} filters={userFilters(type)}>
+    <TableProvider id={`user-list-${type}`} filters={filters}>
       <UserTableContainer type={type} status={status} columns={[]} />
     </TableProvider>
   );
