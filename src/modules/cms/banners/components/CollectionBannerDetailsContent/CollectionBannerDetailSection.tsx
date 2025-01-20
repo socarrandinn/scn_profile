02c52a5e3125c944638dd0add402d8132ec6b0b9
socarrandin <socarrandin@gmail.com>
2025-01-20 16:16:21 -0500

    fix lint

--- src/modules/cms/banners/components/CollectionBannerDetailsContent/CollectionBannerDetailSection.tsx
+++ src/modules/cms/banners/components/CollectionBannerDetailsContent/CollectionBannerDetailSection.tsx
@@ -1,10 +1,8 @@
 import { memo } from 'react';
-import { useCollectionDetails } from 'modules/cms/collections/context/CollectionContext';
-import { useFindCollectionElements } from 'modules/cms/collections/hooks/useFindCollectionElements';
 
 const CollectionBannerDetailSection = () => {
-  const { collectionId } = useCollectionDetails();
-  const data = useFindCollectionElements(collectionId);
+  /* const { collectionId } = useCollectionDetails();
+  const data = useFindCollectionElements(collectionId); */
   return <div></div>;
 };
 
