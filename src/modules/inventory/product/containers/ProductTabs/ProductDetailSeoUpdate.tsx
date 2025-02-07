5a026fe8b9385526bcb58a6db22bf3dc3582acde
Keren <kerensanchezpadron@gmail.com>
2025-02-07 12:02:50 -0500

    [feat]: update import

--- src/modules/inventory/product/containers/ProductTabs/ProductDetailSeoUpdate.tsx
+++ src/modules/inventory/product/containers/ProductTabs/ProductDetailSeoUpdate.tsx
@@ -6,7 +6,7 @@ import { LoadingButton } from '@mui/lab';
 import { useTranslation } from 'react-i18next';
 import useProductSEOCreateForm from 'modules/inventory/product/hooks/useProductSEOCreateForm';
 import { mapGetOneErrors } from 'constants/errors';
-import { ProductSeoForm } from '../../components/ProductSeoForm';
+import ProductSeoForm from 'modules/inventory/product/components/ProductSeoForm/ProductSeoForm';
 
 type productDetailSEOUpdateContainerProps = {
   loadingInitData?: boolean;
