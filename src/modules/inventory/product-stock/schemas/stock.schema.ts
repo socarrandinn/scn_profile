6d548e9665581055679aa2d824558422a8498e69
Keren <kerensanchezpadron@gmail.com>
2025-02-17 12:20:03 -0500

    [fix]: stock warehouse import schema

--- src/modules/inventory/product-stock/schemas/stock.schema.ts
+++ src/modules/inventory/product-stock/schemas/stock.schema.ts
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import '@dfl/yup-validations';
 import { stockCauseSchema } from 'modules/inventory/common/schemas/common-stock.schema';
 import { STOCK_OPERATIONS } from '../constants/stock-operations.constants';
+import { PRODUCT_STOCK_OPERATIONS } from 'modules/inventory/product/constants/stock-operations.constants';
 
 const transformSchema = Yup.string()
   .required('required')
@@ -19,8 +20,20 @@ export const commonSchema = Yup.object().shape({
   warehouseArea: transformSchema,
 });
 
+export const causeSchema = Yup.object().shape({
+  cause: Yup.string()
+    .when('operation', {
+      is: PRODUCT_STOCK_OPERATIONS.DISCOUNTED,
+      then: (schema) => schema.required('required'),
+    })
+    .when('operation', {
+      is: PRODUCT_STOCK_OPERATIONS.ADDED,
+      then: (schema) => schema.transform(() => undefined),
+    }),
+});
+
 // add stock to warehouse
-export const stockWarehouseSchema = Yup.object().concat(commonSchema).concat(stockCauseSchema);
+export const stockWarehouseSchema = Yup.object().concat(commonSchema).concat(causeSchema);
 
 // import stock to warehouse
 export const stockWarehouseImportStockSchema = Yup.object().shape({
