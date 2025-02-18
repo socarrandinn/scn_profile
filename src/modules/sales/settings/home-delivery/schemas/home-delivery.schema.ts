e7b2781e52c0c903fc4a9f1060e34ff8101fbb63
Keren <kerensanchezpadron@gmail.com>
2025-02-18 08:31:07 -0500

    [feat]: update home delivery schema

--- src/modules/sales/settings/home-delivery/schemas/home-delivery.schema.ts
+++ src/modules/sales/settings/home-delivery/schemas/home-delivery.schema.ts
@@ -1,7 +1,24 @@
 import * as Yup from 'yup';
 import '@dfl/yup-validations';
+import { IHomeDelivery } from '../interfaces';
 
 export const homeDeliverySchema = Yup.object().shape({
   name: Yup.string().required('required').min(4, 'min-4').max(255, 'max-255'),
   description: Yup.string().required('required').min(4, 'min-4'),
+});
+
+export const homeDeliveryGlobalSchema: Yup.Schema<IHomeDelivery> = Yup.object().shape({
+  price: Yup.number().required('required').min(0, 'min-0'),
+  time: Yup.object().shape({
+    from: Yup.number().required('required'),
+    to: Yup.number().required('required'),
+  }),
+  weightPrice: Yup.object().shape({
+    price: Yup.number().required('required').min(0, 'min-0'),
+    value: Yup.number().required('required').min(0, 'min-0'),
+  }),
+  volumePrice: Yup.object().shape({
+    price: Yup.number().required('required').min(0, 'min-0'),
+    value: Yup.number().required('required').min(0, 'min-0'),
+  }),
 });
