c4331e188579e9881489ee4876b6ae39e27fc320
Keren <kerensanchezpadron@gmail.com>
2025-02-23 23:44:22 -0500

    [feat]: add city by province table

--- src/modules/sales/settings/common/components/CityByProvinceTable/CityByProvinceTable.tsx
+++ src/modules/sales/settings/common/components/CityByProvinceTable/CityByProvinceTable.tsx
@@ -2,6 +2,8 @@ import { Table } from '@dfl/mui-admin-layout';
 import { useFindHomeDeliveryPlaces } from 'modules/sales/settings/home-delivery/hooks/useFindHomeDeliveryPlaces';
 import { IHomeDelivery } from 'modules/sales/settings/home-delivery/interfaces';
 import { memo } from 'react';
+import { shippingColumns } from '../../constants/shipping-columns';
+import { Box } from '@mui/material';
 
 type Props = {
   row: IHomeDelivery | undefined;
@@ -11,17 +13,18 @@ type Props = {
 const CityByProvinceTable = ({ row }: Props) => {
   const { data, isLoading, error } = useFindHomeDeliveryPlaces();
 
-  console.log('row', row);
   return (
-    <Table
-      key={row?._id}
-      data={[row]}
-      error={error}
-      isLoading={isLoading}
-      total={data?.total || 0}
-      columns={[]}
-      hidePagination
-    />
+    <Box sx={{ '.MuiTableHead-root': { display: 'none', }, paddingLeft: '66px' }}>
+      <Table
+        key={row?._id}
+        data={[row]}
+        error={error}
+        isLoading={isLoading}
+        total={data?.total || 0}
+        columns={shippingColumns}
+        hidePagination
+      />
+    </Box>
   )
 };
 
