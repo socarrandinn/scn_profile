bfb3c293218d48336f77926f4d61e1db49dbf0da
socarrandin <socarrandin@gmail.com>
2025-03-03 10:33:59 -0500

    [porfolio] header page

--- components/containers/page-container.tsx
+++ components/containers/page-container.tsx
@@ -5,11 +5,11 @@ type Props = {
   title: string
 }
 const PageContainer = ({ children, title }: Props & PropsWithChildren) => {
+  
   return (
     <Card className='w-full px-2 md:px-3'>
       <PageHeader variant='compuse' title={title} />
       <CardContent>
-
         {children}
       </CardContent>
     </Card>
