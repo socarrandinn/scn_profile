ffb6012e69a1fd0673636d079b31011ba4dc482a
socarrandin <socarrandin@gmail.com>
2025-03-10 17:09:33 -0400

    causes fix audience

--- src/modules/sales/settings/causes-incidence/components/CausesIncidenceAudienceCell/CausesIncidenceAudienceCell.tsx
+++ src/modules/sales/settings/causes-incidence/components/CausesIncidenceAudienceCell/CausesIncidenceAudienceCell.tsx
@@ -1,15 +1,29 @@
-import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
+import { ICausesIncidence } from '../../interfaces';
+import { useMemo } from 'react';
+import { TagList } from '@dfl/mui-react-common';
 
 type Props = {
-  value: any[];
+  value: ICausesIncidence['notification']['audience'] | [];
 };
 const CausesIncidenceAudienceCell = ({ value }: Props) => {
   const { t } = useTranslation('causesIncidence');
 
+  const list = useMemo(() => {
+    const uniqueTargets = new Set<string>();
+    value?.forEach((item) => {
+      item.target?.forEach((target) => {
+        uniqueTargets.add(t(`fields.target.${target}`));
+      });
+    });
+
+    const listOfTargets = Array.from(uniqueTargets);
+    return listOfTargets;
+  }, [t, value]);
+
   if (!value) return <>-</>;
 
-  return <Typography fontWeight={'bold'}>{`(${value?.length}) ${t('notification.audience')}`}</Typography>;
+  return <TagList value={list} limit={2} />;
 };
 
 export default CausesIncidenceAudienceCell;
