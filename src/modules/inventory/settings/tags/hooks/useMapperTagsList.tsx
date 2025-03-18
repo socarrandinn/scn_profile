c2d356fddf13ee12a9ddde1ef6b195ef7b67fc6b
socarrandin <socarrandin@gmail.com>
2025-03-18 10:04:24 -0400

    fix tag array simple

--- src/modules/inventory/settings/tags/hooks/useMapperTagsList.tsx
+++ src/modules/inventory/settings/tags/hooks/useMapperTagsList.tsx
@@ -1,13 +1,31 @@
 import { useCallback } from 'react';
-import { ISummaryTags } from '../interfaces';
+import { ISummaryTags, TAG_TYPE_ENUM } from '../interfaces';
+import { isArray } from 'lodash';
 
 export const useMapperTagsList = () => {
+  const getArrayValue = (tag: ISummaryTags) => {
+    if (TAG_TYPE_ENUM.ARRAY === tag?.type) {
+      if (isArray(tag?.value)) {
+        if (tag?.value?.length > 1) return tag?.value;
+        return [tag?.value[0]];
+      }
+      return [tag?.value];
+    }
+    return tag?.value;
+  };
+
   const requiredTagList = useCallback((tags?: Record<string, ISummaryTags>) => {
     if (tags) {
-      return Object.values(tags).filter((tag) => tag);
+      return Object.values(tags).filter((tag) => {
+        if (tag?.type === TAG_TYPE_ENUM.ARRAY) {
+          tag.value = getArrayValue(tag);
+        }
+        return tag;
+      });
     }
     return [];
   }, []);
+
   return {
     requiredTagList,
   };
