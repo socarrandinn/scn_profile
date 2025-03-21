6f4fb1180a9a521da5a9c59fe85934606116494b
socarrandin <socarrandin@gmail.com>
2025-03-21 16:47:17 -0400

    fix issues

--- src/utils/comparing.ts
+++ src/utils/comparing.ts
@@ -1,5 +1,4 @@
 export const isOptionEqualToValue = (option: any, value: any) => {
-  console.log(option, value);
   const optionId = option?._id || option;
   const valueId = value?._id || value;
   return optionId === valueId;
