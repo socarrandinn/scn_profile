47f8d33ce4dab2a37ba5a709d3d6e254e14cf30e
socarrandin <socarrandin@gmail.com>
2025-03-31 16:08:08 -0400

    page contact

--- app/[locale]/(home)/contact/page.tsx
+++ app/[locale]/(home)/contact/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 import PageContainer from "@/components/containers/page-container";
 import ContactForm from "@/components/core/contact/contact-form";
-import ContactMap from "@/components/core/contact/contact-map";
 import { CardContent } from "@/components/ui/card";
 import PageHeader from "@/components/ui/page-header";
+import dynamic from "next/dynamic";
+
+const LeafletMap = dynamic(
+  () => import("@/components/core/contact/contact-map"),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
+);
 
 const ContactMePage = () => {
   return (
     <PageContainer title={"contact:section.map"}>
       <CardContent>
         <div className="h-[20rem] w-full">
-          <ContactMap posix={[23.1136, -82.3666]} zoom={12} />
+          <LeafletMap posix={[23.1136, -82.3666]} zoom={12} />
         </div>
       </CardContent>
 
--- components/core/contact/contact-map.tsx
+++ components/core/contact/contact-map.tsx
@@ -7,7 +7,6 @@ import {
   TileLayer,
   useMapEvents,
 } from "react-leaflet";
-
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
