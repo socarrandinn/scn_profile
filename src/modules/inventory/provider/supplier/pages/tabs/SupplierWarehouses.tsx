11ff44d7d63dc76e3e20b3de64d9d60b32d52a4a
Keren <kerensanchezpadron@gmail.com>
2025-04-01 09:33:20 -0400

    [feat]: update filters

--- src/modules/inventory/provider/supplier/pages/tabs/SupplierWarehouses.tsx
+++ src/modules/inventory/provider/supplier/pages/tabs/SupplierWarehouses.tsx
@@ -1,6 +1,6 @@
 import { TableProvider } from '@dfl/mui-admin-layout';
 import { PagePaperLayout } from 'layouts/index';
-import { warehouseFilters } from 'modules/inventory/warehouse/constants';
+import { supplierWarehouseFilters } from 'modules/inventory/warehouse/constants';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import SupplierWarehouseListContainer from '../../containers/SupplierWarehouseListContainer';
@@ -10,7 +10,7 @@ const SupplierWarehouses = () => {
   
   return (
     <PagePaperLayout title={t('list')} mb={3}>
-      <TableProvider id={'supplier-warehouses'} filters={warehouseFilters}>
+      <TableProvider id={'supplier-warehouses'} filters={supplierWarehouseFilters}>
         <SupplierWarehouseListContainer />
       </TableProvider>
     </PagePaperLayout>
