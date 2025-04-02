be60cbeb4327352e2f8ca19bdc384c6330bad253
socarrandin <socarrandin@gmail.com>
2025-04-01 21:42:42 -0400

    fix summary

--- components/containers/profile-social-network.tsx
+++ components/containers/profile-social-network.tsx
@@ -1,23 +1,30 @@
-
-import { socials } from '@/constants/social'
-import { ISocial } from '../../definitions/Profile'
+import { socials } from "@/constants/social";
+import { ISocial } from "../../definitions/Profile";
 
 const ProfileSocialNetwork = () => {
   return (
-    <div className='[&>svg]:text-color flex flex-row gap-3'>
-      {socials?.map(social => <SocialItem key={social?.label} social={social} />)}
+    <div className="[&>svg]:text-color flex flex-row gap-3 px-6 py-3 rounded-md bg-slate-400/30 backdrop-blur-sm">
+      {socials?.map((social) => (
+        <SocialItem key={social?.label} social={social} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSocialNetwork
+export default ProfileSocialNetwork;
 
 type Props = {
-  social: ISocial
-}
+  social: ISocial;
+};
 const SocialItem = ({ social }: Props) => {
-
   return (
-    <a className='hover:text-primary hover:[&>svg]:stroke-primary' href={social.href} target="_blank" rel="noopener noreferrer">{social?.icon}</a>
-  )
-}
+    <a
+      className="hover:text-primary hover:[&>svg]:stroke-primary"
+      href={social.href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {social?.icon}
+    </a>
+  );
+};
