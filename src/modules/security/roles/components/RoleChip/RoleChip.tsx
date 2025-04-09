79001a0c0d06db1738e1ab94147d02c62bc61e73
Keren <kerensanchezpadron@gmail.com>
2025-04-09 16:05:48 -0400

    [feat]: update roles link in user and providers list

---
+++ src/modules/security/roles/components/RoleChip/RoleChip.tsx
@@ -0,0 +1,18 @@
+import { Chip } from '@mui/material';
+import { RoleCellProps } from '../RoleCell/RoleCell';
+import { RoleAvatar } from '../RoleAvatar';
+import { ReactLink } from '@dfl/react-security';
+
+export const RoleChip = ({ role, bgColor, route }: RoleCellProps) => {
+  return (
+    <Chip
+      avatar={<RoleAvatar role={role} size={24} bgColor={bgColor} />}
+      label={
+        <ReactLink to={`/security/roles/${route}/${(role?.role || role?._id) as string}`} underline={'hover'}>
+          {role?.name}
+        </ReactLink>
+      }
+      variant='outlined'
+    />
+  );
+};
--- src/modules/security/users/components/RolesCell/RolesCell.tsx
+++ src/modules/security/users/components/RolesCell/RolesCell.tsx
@@ -1,9 +1,10 @@
 import { styled } from '@mui/material';
 import { IRoleSetting } from 'modules/security/users/interfaces/IRoleSetting';
-import { RoleChip } from 'modules/security/roles/components/RoleCell/RoleCell';
 import { renderTagList } from '@dfl/mui-react-common';
 import { IRole } from 'modules/security/roles/interfaces';
 import { useMemo } from 'react';
+import { RoleChip } from 'modules/security/roles/components/RoleChip';
+import { ROLE_TYPE_ENUM } from 'modules/security/roles/constants/role-provider.enum';
 
 const RolesCellStyle = styled('div')(({ theme }) => ({
   '.MuiPaper-root': {
@@ -17,12 +18,15 @@ const RolesCellStyle = styled('div')(({ theme }) => ({
 }));
 
 export const RoleChipCell = ({ text }: { text?: IRole }) => {
-  const isProvider = useMemo(() => !!text?.provider, [text]);
+  console.log(text);
+  const isProvider = useMemo(() => text?.type !== ROLE_TYPE_ENUM.ROOT, [text]);
+
+  const route = useMemo(() => (isProvider ? 'providers' : 'system'), [isProvider]);
 
   if (!text) return <></>;
-  return <RoleChip role={text} bgColor={isProvider ? 'warning' : 'error'} />;
+  return <RoleChip role={text} bgColor={isProvider ? 'warning' : 'error'} route={route} />;
 };
 
-export const RolesCell = ({ roles }: { roles: IRoleSetting[] }) => {
+export const RolesUserCell = ({ roles }: { roles: IRoleSetting[] }) => {
   return <RolesCellStyle>{renderTagList(roles, 2, RoleChipCell)}</RolesCellStyle>;
 };
