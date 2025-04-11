6a11d130c07b9192f80da66ff8ba98b8e06617a6
Keren <kerensanchezpadron@gmail.com>
2025-04-11 15:12:38 -0400

    [fix]: payment gateways  and client history

--- src/modules/crm/clients/pages/tabs/ClientHistoryChange.tsx
+++ src/modules/crm/clients/pages/tabs/ClientHistoryChange.tsx
@@ -2,7 +2,7 @@ import { TableProvider } from '@dfl/mui-admin-layout';
 import { auditLogFilters } from 'modules/security/audit-logs/constants';
 import AuditLogHistoryChangeContainer from 'modules/security/audit-logs/containers/AuditLogHistoryChangeContainer';
 import { AuditLogEntityProvider } from 'modules/security/audit-logs/context/AuditLogEntityContext';
-import { useFindAuditLogsByUser } from 'modules/security/audit-logs/hooks/useFindAuditLogsByUser';
+import { useFindAuditLogsByEntity } from 'modules/security/audit-logs/hooks/useFindAuditLogsByEntity';
 import { memo } from 'react';
 import { useParams } from 'react-router';
 
@@ -11,7 +11,7 @@ const ClientHistoryChange = () => {
 
   return (
     <TableProvider id={'client-history-change'} filters={auditLogFilters}>
-      <AuditLogEntityProvider entityId={id as string} useHook={useFindAuditLogsByUser}>
+      <AuditLogEntityProvider entityId={id as string} useHook={useFindAuditLogsByEntity}>
         <AuditLogHistoryChangeContainer />
       </AuditLogEntityProvider>
     </TableProvider>
