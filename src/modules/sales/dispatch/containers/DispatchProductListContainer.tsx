eac3dd94c32115bb45b8d85494405546a0e7cad5
socarrandin <socarrandin@gmail.com>
2025-04-14 14:15:42 -0400

    quit toolbar

--- src/modules/sales/dispatch/containers/DispatchProductListContainer.tsx
+++ src/modules/sales/dispatch/containers/DispatchProductListContainer.tsx
@@ -1,16 +1,15 @@
 import { memo } from 'react';
 import { Table } from '@dfl/mui-admin-layout';
 import Box from '@mui/material/Box';
-import { DispatchSubOrderListToolbar } from '../components/DispatchSubOrderListToolbar';
 import { useFindDispatchProducts } from '../hooks/useDispatchTabs';
 import { productColumns } from 'modules/inventory/product/constants';
 import { dispatchProductColumn } from '../constants';
 
 const DispatchProductListContainer = () => {
-  const { isLoading, error, data, filters, search } = useFindDispatchProducts();
+  const { isLoading, error, data } = useFindDispatchProducts();
   return (
     <Box>
-      <DispatchSubOrderListToolbar filters={filters} total={data?.total} search={search as string} />
+      {/*   <DispatchSubOrderListToolbar filters={filters} total={data?.total} search={search as string} /> */}
       <Table
         columns={dispatchProductColumn(productColumns?.filter((column) => !(column.field as string)?.match(/actions/)))}
         data={data?.data}
--- src/modules/sales/dispatch/containers/DispatchWarehouseListContainer.tsx
+++ src/modules/sales/dispatch/containers/DispatchWarehouseListContainer.tsx
@@ -1,16 +1,15 @@
 import { memo } from 'react';
 import { Table } from '@dfl/mui-admin-layout';
 import Box from '@mui/material/Box';
-import { DispatchSubOrderListToolbar } from '../components/DispatchSubOrderListToolbar';
 import { useFindDispatchWarehouses } from '../hooks/useDispatchTabs';
 import { dispatchWarehouseColumn } from '../constants';
 import { warehouseColumns } from 'modules/inventory/warehouse/constants';
 
 const DispatchWarehouseListContainer = () => {
-  const { isLoading, error, data, filters, search } = useFindDispatchWarehouses();
+  const { isLoading, error, data } = useFindDispatchWarehouses();
   return (
     <Box>
-      <DispatchSubOrderListToolbar filters={filters} total={data?.total} search={search as string} />
+      {/* <DispatchSubOrderListToolbar filters={filters} total={data?.total} search={search as string} /> */}
       <Table
         columns={dispatchWarehouseColumn(
           warehouseColumns?.filter((column) => !(column.field as string)?.match(/actions/)),
